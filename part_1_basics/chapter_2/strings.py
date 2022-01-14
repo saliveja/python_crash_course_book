feec7e49c305fad5f36bc45eaa8e3df6a7286785
saliveja <esman@riseup.net>
2022-01-14 05:46:05 +0100

    Strings whitespace

--- part_1_basics/chapter_2/strings.py
+++ part_1_basics/chapter_2/strings.py
@@ -19,3 +19,13 @@ print("Languages:\nPython\nC\nJavaScript")
 print("Python\n\nPython")
 
 print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+favorite_language = 'python '
+
+print(favorite_language)
+
+print(favorite_language.rstrip())
+
+print(favorite_language)
+
+
