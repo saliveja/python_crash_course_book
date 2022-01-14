e354377ab26ab1e4634cbe9b18477731ce70fcce
saliveja <esman@riseup.net>
2022-01-14 05:59:23 +0100

    Strings whitespace update

--- part_1_basics/chapter_2/strings.py
+++ part_1_basics/chapter_2/strings.py
@@ -29,3 +29,11 @@ print(favorite_language.rstrip())
 print(favorite_language)
 
 
+favorite_language = 'python '
+favorite_language = favorite_language.rstrip()
+print(favorite_language)
+
+favorite_language = ' python '
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
