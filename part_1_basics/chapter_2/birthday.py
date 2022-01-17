9fd5c6a5cfccf8c07e4b0bdbaefec459a4e529e5
saliveja <esman@riseup.net>
2022-01-17 16:39:56 +0100

    birthday continue

--- part_1_basics/chapter_2/birthday.py
+++ part_1_basics/chapter_2/birthday.py
@@ -1,6 +1,11 @@
 
 age = 23
-message = "Happy " + str(age) + "rd birthday!"
+message = "Happy " + str(age) + "rd birthday!\n"
 print(message)
+
+age = 60
+name = "Kerstin Eriksson"
+message = "\tHappy " + str(age) + "th birthday "
+print(message + name.title() + "!")
 
 
