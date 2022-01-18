5623cbdd35c2b07940f4b29c1ae74880626ca37e
saliveja <esman@riseup.net>
2022-01-18 17:45:42 +0100

    favorite number message

--- part_1_basics/chapter_2/favorite_number.py
+++ part_1_basics/chapter_2/favorite_number.py
@@ -1,3 +1,7 @@
 favorite_number = str(1000 + 20*45 + 7*3)
-print(favorite_number)
+print(favorite_number + "\n")
 
+
+favorite_number = "1936"
+message = "If I guess, I think " + str(favorite_number) + " is both your favorite number and year!"
+print(message)
