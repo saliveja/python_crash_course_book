2ece8984d1150f2877223c54776e6466c4d67d5d
saliveja <esman@riseup.net>
2022-01-18 17:59:01 +0100

    comment first commit

---
+++ part_1_basics/chapter_2/comment.py
@@ -0,0 +1,8 @@
+# say hello to everyone.
+print("Hello Python People!")
+
+
+name = "Elsa"
+print(name.upper())
+
+# after writing the code about name changes it can be useful to share what didn't work for example I often forget to put () after name.upper which leads to an error message. I will from now on, share the thoughts, what worked and didn't within the program.
