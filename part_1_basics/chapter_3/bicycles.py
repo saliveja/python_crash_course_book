b2f0c75f08b15a74d68717d8f13a6bdd935d7eed
saliveja <esman@riseup.net>
2022-01-19 10:52:45 +0100

    names list

--- part_1_basics/chapter_3/bicycles.py
+++ part_1_basics/chapter_3/bicycles.py
@@ -8,6 +8,20 @@ print(bicycles[0] + "\n")
 bicycles = ['trek' , 'cannondale' , 'redline' , 'specialized']
 print(bicycles[0].title() + "\n")
 
+# index is number 0 in python
 
 bicycles = ['trek' , 'cannondale' , 'redline' , 'specialized']
 print(bicycles[3].lower())
+
+bicycles = ['trek' , 'cannondale' , 'redline' , 'specialized']
+print(bicycles[1])
+print(bicycles[3])
+
+bicycles = ['trek' , 'cannondale' , 'redline' , 'specialized']
+print(bicycles[-1])
+
+# -1 shows the last element on the list
+
+bicycles = ['trek' , 'cannondale' , 'redline' , 'specialized']
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
---
+++ part_1_basics/chapter_3/names_list.py
@@ -0,0 +1,7 @@
+
+
+names_list = ['Louise' , 'Emma' , 'Marusha' , 'Lucy']
+print(names_list[0].lower() + "\n")
+print(names_list[1].upper() + "\n")
+print(names_list[2].title() + "\n")
+print(names_list[-1])
