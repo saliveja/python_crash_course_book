4b5bee5aced3a7fe014e0487046c5fd378d26403
saliveja <esman@riseup.net>
2022-01-19 10:00:23 +0100

    git and python notes first commit

--- part_1_basics/chapter_2/comment.py
+++ part_1_basics/chapter_2/comment.py
@@ -6,4 +6,4 @@ print("Hello Python People!")
 name = "Elsa"
 print(name.upper())
 
-# after writing the code about name changes it can be useful to share what didn't work for example I often forget to put () after name.upper which leads to an error message. I will from now on, share the thoughts, what worked and didn't within the program. I also see that there is an inconsistency in how I name commits.
+# after writing the code about name changes it can be useful to share what didn't work for example I often forget to put () after name.upper which leads to a name error message. I will from now on, share the thoughts, what worked and didn't within the program. I also see that there is an inconsistency in how I name commits.
