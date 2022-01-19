9e036d96509391952b8d46af45c6b6711d1caa79
saliveja <esman@riseup.net>
2022-01-19 10:28:11 +0100

    lists first commit and python notes update

---
+++ part_1_basics/chapter_3/bicycles.py
@@ -0,0 +1,13 @@
+
+bicycles = ['trek' , 'cannondale' , 'redline' , 'specialized']
+print(bicycles)
+
+bicycles = ['trek' , 'cannondale' , 'redline' , 'specialized']
+print(bicycles[0] + "\n")
+
+bicycles = ['trek' , 'cannondale' , 'redline' , 'specialized']
+print(bicycles[0].title() + "\n")
+
+
+bicycles = ['trek' , 'cannondale' , 'redline' , 'specialized']
+print(bicycles[3].lower())
