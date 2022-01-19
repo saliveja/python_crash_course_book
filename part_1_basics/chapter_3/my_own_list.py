2c5f81960b67a5dce9106cf2b15ec97f74074a3f
saliveja <esman@riseup.net>
2022-01-19 11:47:41 +0100

    my own list

---
+++ part_1_basics/chapter_3/my_own_list.py
@@ -0,0 +1,12 @@
+
+transport = ['walking' , 'train' , 'bicycle' , 'run' , 'airplane']
+message = "When I was a child I used to " + transport[3] + " everywhere. I wanted to be with my sister, but she wanted to get rid of me. So she took the " + transport[2] + " and while she was going as fast as she could, I was running behind her back and forth on our countryside road." + "\n" + "\n"
+print(message) 
+
+transport = ['walking' , 'train' , 'bicycle' , 'run' , 'airplane'] 
+message = "I like to see new places, but I do not enjoy traveling far by " + transport[-1] + " anymore. It takes to long time to get back into a routine and the jetlag is making me incedible tired." + "\n" + "\n"
+print(message)
+
+transport = ['walking' , 'train' , 'bicycle' , 'run' , 'airplane'] 
+message = transport[0].title() + " in the forest helps be to feel grounded and to solve problems in everyday life. I cannot think with all the noise of the city"
+print(message)
