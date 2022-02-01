c570b08434c9c7959e18f7fa7b451b36857ee287
saliveja <esman@riseup.net>
2022-02-01 12:34:00 +0100

    try it yourself 3-4 guest list update with comments

--- part_1_basics/chapter_3/try_it_yourself_3_4_guest_list.py
+++ part_1_basics/chapter_3/try_it_yourself_3_4_guest_list.py
@@ -4,9 +4,11 @@ print(guests)
 
 guests.append('Sandra')
 print(guests)
+# adding guest at the end of the list.
 
 guests.insert(1, 'Elsa')
 print(guests)
+# inserting guest at position 1 in the list.
 
 print("Dear " + guests[1] + ", I'm putting together a dinner on Sunday and hope you'll have time to come. " + guests[0] + " will also come and I know you wanted to se each other for some time. Hope to see you then <3" + "\n") 
 
@@ -18,6 +20,7 @@ print("My sweet " + guests[3] + ", " + message + "\n")
 
 message = "you are welcome for dinner on Sunday at our place at 1900. If it's a nice day we'll be outside. Send a text so we know if you can come or not. Greetings"
 print("Dear " + guests[4] + " and " + guests[5] + ", " + message)
+# writing in plural even if it's a list can create unnecessary syntax errors, ie. writing guest instead of guests.
 
 guests = ['Kerstin' , 'Elsa' , 'Erika' , 'mother' , 'beloved comrade' , 'Marie' , 'Birgitta' , 'Sandra']
 print(guests)
