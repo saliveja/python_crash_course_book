934864945010245f53ee45dfbd011fb664590dc7
saliveja <esman@riseup.net>
2022-02-01 12:02:47 +0100

    try it yourself guest list 3-4

--- part_1_basics/chapter_3/try_it_yourself_3_4_guest_list.py
+++ part_1_basics/chapter_3/try_it_yourself_3_4_guest_list.py
@@ -14,12 +14,13 @@ message = "you are welcome for dinner on Sunday at our place at 1900. If it's a 
 print("Dear " + guests[0].title() + ", " + message + "\n")
 
 message = "you are welcome for dinner on Sunday at our place at 1900. If it's a nice day we'll be outside. Send a text so we know if you can come or not. Greetings"
-print("My sweet " + guests[3] + ", " + message)
-
-not_coming_1 = guests.pop(4)
-print(guests)
-message = "I so much wanted to see you and was hoping you would have time to come for dinner on Sunday, but I heard that are traveling at the moment. Give me a call when you are back."
-print(not_coming_1 + ", " + message + "\n")
+print("My sweet " + guests[3] + ", " + message + "\n")
 
 message = "you are welcome for dinner on Sunday at our place at 1900. If it's a nice day we'll be outside. Send a text so we know if you can come or not. Greetings"
 print("Dear " + guests[4] + " and " + guests[5] + ", " + message)
+
+guests = ['Kerstin' , 'Elsa' , 'Erika' , 'mother' , 'beloved comrade' , 'Marie' , 'Birgitta' , 'Sandra']
+print(guests)
+comrade_not_coming_1 = guests.pop(4)
+print(guests)
+print("Our " + comrade_not_coming_1 + ", Jiyan, will unfortunately not be able to come due to travels.")
