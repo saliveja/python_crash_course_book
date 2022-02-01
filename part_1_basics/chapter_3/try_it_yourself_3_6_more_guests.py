89246b13efdf19a2fda15edee9a733e0f1702f86
saliveja <esman@riseup.net>
2022-02-01 12:38:31 +0100

    try it yourself 3-6 more guests first commit

---
+++ part_1_basics/chapter_3/try_it_yourself_3_6_more_guests.py
@@ -0,0 +1,12 @@
+
+guests = ['Kerstin' , 'Elsa' , 'Erika' , 'mother' , 'beloved comrade' , 'Marie' , 'Birgitta' , 'Sandra']
+print(guests)
+
+guests.insert(0, 'Karin')
+print(guests)
+
+guests.insert(5, 'Linnea')
+print(guests)
+
+guests.append('Canfeda')
+print(guests)
