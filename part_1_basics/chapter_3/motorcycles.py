af339fc276e70c528375bdc23558669dd284079e
saliveja <esman@riseup.net>
2022-02-01 09:34:35 +0100

    Motorcycles pop update

--- part_1_basics/chapter_3/motorcycles.py
+++ part_1_basics/chapter_3/motorcycles.py
@@ -55,3 +55,6 @@ motorcycles = ['honda' , 'yamaha' , 'suzuki']
 last_owned = motorcycles.pop()
 print("The last motorcycles I owned was a " + last_owned.title() + ".")
 
+first_owned = motorcycles.pop(0)
+print("The fist motorcycle I owned was a " + first_owned.title() + ".")
+
