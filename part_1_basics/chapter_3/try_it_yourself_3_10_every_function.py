1d933efa9b4540fbe1dca997f517b9bbc7213894
saliveja <esman@riseup.net>
2022-02-02 12:09:07 +0100

    try it yourself 3-10 delete

--- part_1_basics/chapter_3/try_it_yourself_3_10_every_function.py
+++ part_1_basics/chapter_3/try_it_yourself_3_10_every_function.py
@@ -27,7 +27,7 @@ print(message + str(shoppinglist))
 shoppinglist.insert(3,'apples')
 shoppinglist.insert(0, 'almond drink')
 print(shoppinglist)
-# inserting item at position 3 on the list
+# inserting apples at position 3 and almond drink at position 0 on the list
 
 message = "\n\n"
 shoppinglist.sort(reverse=True)
@@ -36,11 +36,6 @@ print(message + str(shoppinglist))
 
 print("The " + shoppinglist[1] + " didn't arrive yet.")
 
-
-<<<<<<< HEAD
-
-
-=======
 already_arrived = 'eggs'
 	# QUESTION:
 	# How to add more items, so they get bth printed as "arrived"
@@ -49,4 +44,9 @@ already_arrived = 'eggs'
 shoppinglist.remove(already_arrived)
 print("\n\nThese items already arrived: " + already_arrived + ".")
 print("\n\nThis is the current version of the list:\n" + str(shoppinglist) + "\nThere are " + str(len(shoppinglist)) + " items left to be aquired.")
->>>>>>> 99a38ef1362cc86d0b39c2667329f63084327998
+# removing('eggs') gives the possibility to remove an item without knowing it's position.
+
+print(shoppinglist [-1])
+del shoppinglist[-1]
+print(shoppinglist) 
+
