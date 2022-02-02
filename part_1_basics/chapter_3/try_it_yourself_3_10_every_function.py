254a9a34bd8bed4d0c83a36f769d5cc213120968
saliveja <esman@riseup.net>
2022-02-02 11:31:35 +0100

    try it yourself 3-10 every function update

--- part_1_basics/chapter_3/try_it_yourself_3_10_every_function.py
+++ part_1_basics/chapter_3/try_it_yourself_3_10_every_function.py
@@ -2,6 +2,10 @@
 shoppinglist = ['eggs', 'milk', 'cheese', 'yogurt', 'almond butter', 'butter', 'soda water', 'coconut cream', 'broccoli', 'onion', 'cauliflower', 'mushrooms', 'carrots', 'garlic', 'ginger', 'earl gay', 'coffee', 'cream']
 print(shoppinglist)
 
+shoppinglist.append('sweet potatoes')
+print(shoppinglist)
+# adding item at the end of the list
+
 message = "\n\n" + str(sorted(shoppinglist))
 print(message)
 
@@ -13,6 +17,25 @@ shoppinglist.reverse()
 message = "\n\nThis is the shoppinglist for this week!"
 print(message)
 print(shoppinglist)
+# reverse() is permanent
+
+message = "\n\n"
+shoppinglist.sort()
+print(message + str(shoppinglist))
+# to put things together (for example add a message to our list, we need to use str. print(message + shoppinglist) wouldn't be an understandable command.
+
+shoppinglist.insert(3,'apples')
+shoppinglist.insert(0, 'almond drink')
+print(shoppinglist)
+# inserting item at position 3 on the list
+
+message = "\n\n"
+shoppinglist.sort(reverse=True)
+print(message + str(shoppinglist))
+# sort() is permanent
+
+print("The " + shoppinglist[1] + " didn't arrive yet.")
+
 
 
 
