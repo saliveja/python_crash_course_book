4d85822feb6b506be5325fb987bbe5af3ed1ebd5
saliveja <esman@riseup.net>
2022-02-03 13:38:08 +0100

    numbers trying random stuff with lists

--- part_1_basics/chapter_4/numbers.py
+++ part_1_basics/chapter_4/numbers.py
@@ -5,3 +5,26 @@ for value in range(1,5):
 
 for value in range(1,6):
 	print(value)
+	print("\n")
+
+numbers = list(range(1,6))
+print(numbers)
+
+
+members = ['anna', 'sven', 'danny', 'marie', 'olga']
+for member in members:
+	for value in range(1,6):
+		print(member)
+# the value loop inside om member defines how many times each name should be printed
+
+for value in range(1,51):
+	print(value)
+	names = ['anna', 'sven', 'danny', 'marie', 'olga']
+	for name in names:
+		print(name.title())
+		# This prints the five name under every number
+		
+	
+
+
+
