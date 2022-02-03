402cb0fddd0ba9eafd27daa7c4203f7524ce1c51
saliveja <esman@riseup.net>
2022-02-03 11:49:03 +0100

    try it yourself 4-2 animals first commit

---
+++ part_1_basics/chapter_4/try_it_yourself_4_2_animals.py
@@ -0,0 +1,9 @@
+
+animals = ['cat', 'bird', 'fish']
+for animal in animals:
+	print(animal.title())
+	print("To keep a " + animals[1] + " in a cage goes again my fundamental values\n")
+	print(animals.index('fish'))
+	print("I my family we used to go on trips in the winter with the snow mobile to " + animals[2] + "\n")
+	print("In our squat, there was a " + animals[0] + " mama who gave birth to her kittens under our stairs.")
+
