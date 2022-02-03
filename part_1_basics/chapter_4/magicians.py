12ac4130411cb175bbcfa61802333968dbc958ff
saliveja <esman@riseup.net>
2022-02-03 10:57:05 +0100

    magicians update

--- part_1_basics/chapter_4/magicians.py
+++ part_1_basics/chapter_4/magicians.py
@@ -8,16 +8,16 @@ for magician in magicians:
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
 	print(magician.title() + ", that was a great trick!")
-	# if we don't define the list again, the name will be sorted alphabetcially.
+	# if we don't define the list again, the name will be sorted alphabetcially
 	
 	print("I can't wait to see your next trick, " + magician.title() + ".\n")
 	# we can write as many lines as we wish in the for loop
 	
 print("Thank you, everyone. That was a great magic show!")
-# this print concludes the loop. It's not a part of the loop as it is not a part of the indent. It will be printed only once.
-# indentation is used to show that one line is connected to another. 
+# this print concludes the loop (not a part of indent
+# It will be printed only once
+# indentation is used to show that one line is connected to another
 # indentation also help with organizing code
-
 
 for magician in magicians:
 print(magician)
@@ -27,9 +27,23 @@ magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
 	print(magician.title() + ", that was a great trick!")
 print("I can't wait to see your next trick, " + magician.title() + ".\n")
-# Geany reports all indentation errors, and will not print the line explained in the book. The last print will refer to last known information which is to print message including magician Carolina and look like this:
+# Geany reports all indentation errors, and will not print the line explained in the book
+# The last print will refer to last known information
+# It will lokk like this:
 
 # Alice, that was a great trick!
 # David, that was a great trick!
 # Carolina, that was a great trick!
-# I can't wait to see your next trick, Carolina.
+# I can't wait to see your next trick, Carolina
+
+message = "Hello Python world!"
+	print(message)
+	# python communicates that it's an unexpected indent
+	
+magicians = ['alice', 'david', 'carolina']
+for magicians in magicians:
+	print(magician.title() + ", that was a great trick!")
+	print("I can't wait to see your next trick, " + magician.title() + ".\n")
+	
+	print("Thank you everyone, that was a great magic show!")
+	# because the last line is indented, it will be printed for each magician in the list
