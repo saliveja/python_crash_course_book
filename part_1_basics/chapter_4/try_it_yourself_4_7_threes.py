451cada1fce93927a871d2c52254268ce18b6a4b
saliveja <esman@riseup.net>
2022-02-04 13:12:23 +0100

    try it yourself 4-8 first commit

--- part_1_basics/chapter_4/try_it_yourself_4_7_threes.py
+++ part_1_basics/chapter_4/try_it_yourself_4_7_threes.py
@@ -10,3 +10,4 @@ threes = [value*3 for value in range(1,31)]
 for three in threes:
 	print(three)
 	# trying more concise code to print numbers 1-30 multiplied by three
+	# with two for commands, is it a loop in a loop? how many can be used at one time?
