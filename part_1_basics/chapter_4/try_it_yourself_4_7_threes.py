8b533047527f91897e0f624f1efc350d2051fb5d
saliveja <esman@riseup.net>
2022-02-04 13:45:27 +0100

    try it yourself 4-7 updated to correct the exercise

--- part_1_basics/chapter_4/try_it_yourself_4_7_threes.py
+++ part_1_basics/chapter_4/try_it_yourself_4_7_threes.py
@@ -1,12 +1,12 @@
 
 threes = []
-for value in range(1,31):
-	threes.append(value*3)
+for value in range(3,31,3):
+	threes.append(value)
 print(threes)	
 # using a for loop to print numbers 1-30 multiplied by three 
 
 
-threes = [value*3 for value in range(1,31)]
+threes = [value for value in range(3,31,3)]
 for three in threes:
 	print(three)
 	# trying more concise code to print numbers 1-30 multiplied by three
