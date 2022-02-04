045d526cd1d482a18d868c144400e49df204b701
saliveja <esman@riseup.net>
2022-02-04 11:48:38 +0100

    try it yourself 4-6 first commit

---
+++ part_1_basics/chapter_4/try_it_yourself_4_5_summing_a_million.py
@@ -0,0 +1,9 @@
+
+
+one_million = list(range(1,1000000))
+min(one_million)
+max(one_million)
+sum(one_million)
+# making a list of numbers 1-1000000
+
+
---
+++ part_1_basics/chapter_4/try_it_yourself_4_6_odd_numbers.py
@@ -0,0 +1,5 @@
+
+odd_numbers = []
+for value in range(3,21,3):
+	odd_numbers.append(value)
+print(odd_numbers)
