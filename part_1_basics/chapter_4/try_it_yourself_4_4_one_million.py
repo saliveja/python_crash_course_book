97901474866ef81856ca54b0db4ac8077540af10
saliveja <esman@riseup.net>
2022-02-04 13:35:30 +0100

    try it yourself 4-4 editing notes

--- part_1_basics/chapter_4/try_it_yourself_4_4_one_million.py
+++ part_1_basics/chapter_4/try_it_yourself_4_4_one_million.py
@@ -4,8 +4,7 @@ for value in range(1, 1000001):
 	one_million.append(value)
 print(one_million)	
 # using loop to print a list of numbers 1-1000000
-# the numbers start printing at 900069. Where is the mistake?
-
+# need to change preferences in terminal to show numbers further back
 
 	
 	
