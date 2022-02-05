fcec7f83e24291374424d089476a063daddf4ba9
saliveja <esman@riseup.net>
2022-02-05 10:56:33 +0100

    try it yourself 4-10 updating with notes

--- part_1_basics/chapter_4/try_it_yourself_4_10_slices.py
+++ part_1_basics/chapter_4/try_it_yourself_4_10_slices.py
@@ -4,14 +4,16 @@ print("The first three items in the list are:")
 for item in items[:3]:
 	print(item.title())
 print("\n\n")
+# printing the first three items on the list
 
 print("Three items from the middle of the list are:")
 for item in items[4:7]:
 	print(item.title())
 print("\n\n")
+# printing three items on index 4, 5 and 6
 
 print("the last three items in the list are:")
 for item in items[-3:]:
 	print(item.title())
-	
+	# printing the last three items on the list
 
