7e868f7e3e014d34def442471b4a8206c3d9952b
saliveja <esman@riseup.net>
2022-02-05 09:29:26 +0100

    players omitted beginning and end

--- part_1_basics/chapter_4/players.py
+++ part_1_basics/chapter_4/players.py
@@ -3,4 +3,20 @@ players = ['charles', 'martina', 'michael', 'florence', 'eli']
 print(players[0:3])
 # print players 1, 2 and 3
 
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[1:4])
+
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[:4])
+# printing the first four
+# It is included as in last exercise, the last number is not printed
+# when first index isn't specified it starts from the beginning of the list
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[2:])
+# when the index at the end is not specified - 
+# it will be printed until the end of the list  
+
+
 
