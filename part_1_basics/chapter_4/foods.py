a772c65dac2214e2aec3c33a400608488eff9598
saliveja <esman@riseup.net>
2022-02-05 10:27:57 +0100

    updating notes

--- part_1_basics/chapter_4/foods.py
+++ part_1_basics/chapter_4/foods.py
@@ -2,9 +2,16 @@
 my_foods = ['pizza', 'falafel', 'carrot cake']
 friend_foods = my_foods[:]
 # [:] refers to the whole my_foods list
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+# adding a new item on each list
+
 print("My favorite foods are:")
 print(my_foods)
 
 print("\nMy friend's favorite foods are:")
 print(friend_foods)
 # the two prints show the same list because one = the other
+
+
