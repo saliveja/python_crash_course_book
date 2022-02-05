ff9cc23c794f4bdf8d867fd9367f5b6edb3f3de5
saliveja <esman@riseup.net>
2022-02-05 11:13:09 +0100

    try it yourself 4-11 printing statement and list with for loop

--- part_1_basics/chapter_4/try_it_yourself_4_11_my_pizzas_your_pizzas.py
+++ part_1_basics/chapter_4/try_it_yourself_4_11_my_pizzas_your_pizzas.py
@@ -1,13 +1,23 @@
 
 pizzas = ['vezuvio', 'hawaii', 'calzone']
 friend_pizzas = pizzas[:]
+# this defines that pizzas and friend_pizzas look the same
 
 pizzas.append('detroit')
 print(pizzas)
+# adding detroit to pizzas
 
 friend_pizzas.append('st louis')
 print(friend_pizzas)
+# adding st louis to friend_pizzas
 
 print("My favorite pizzas are:")
 for pizza in pizzas[:]:
 	print(pizza.title())
+print("\n\n")
+# printing statement and the list pizzas in a for loop
+
+print("My friend's favorite pizzas are:")
+for friend_pizza in friend_pizzas:
+	print(friend_pizza.title())
+	# printing statement about the friends favorite pizzas in a for loop.
