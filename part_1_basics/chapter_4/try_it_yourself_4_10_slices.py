e9ca89081bf6f232272a8b932fa9cba751c1b174
saliveja <esman@riseup.net>
2022-02-05 10:51:18 +0100

    try it yourself 4-10 printing from the middle of the list

--- part_1_basics/chapter_4/try_it_yourself_4_10_slices.py
+++ part_1_basics/chapter_4/try_it_yourself_4_10_slices.py
@@ -3,3 +3,8 @@ items = ['hairbrush', 'tshirt', 'pants', 'socks', 'toothbrush', 'sweater', 'hood
 print("The first three items in the list are:")
 for item in items[:3]:
 	print(item.title())
+print("\n\n")
+
+print("Three items from the middle of the list are:")
+for item in items[4:7]:
+	print(item.title())
