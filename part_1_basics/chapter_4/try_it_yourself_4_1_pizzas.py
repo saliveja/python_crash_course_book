011dff5eb613cd2af59d3dfcebdead38b9b08f82
saliveja <esman@riseup.net>
2022-02-06 11:22:44 +0100

    try it yourself pizzas organized with pep standards

--- part_1_basics/chapter_4/try_it_yourself_4_1_pizzas.py
+++ part_1_basics/chapter_4/try_it_yourself_4_1_pizzas.py
@@ -5,10 +5,12 @@ for pizza in pizzas:
 	print(pizza.title() + ", is a funny name for a pizza.\n")
 
 print("When I was a child, I really loved " + pizzas[0].title())
-print("I don't really remember " + pizzas[pizzas.index('calzone')].title() + ", but there is one which is folded that is great!")
+print("I don't really remember " + pizzas[pizzas.index('calzone')].title() + \
+", but there is one which is folded that is great!")
 # how to point to an item on the list which we don't know the position of?
 
 print(pizzas.index('hawaii'))
 # to know the index number of a specific item
-# list[list.index('nameofitem')] --> if we want to print the name directly in the message
-print("Fruit on pizzas like " + pizzas[1] + " is not something that I would recommend.")
+# list[list.index('nameofitem')] --> printing the name directly in the message
+print("Fruit on pizzas like " + pizzas[1] + " is not something that I would \
+recommend.")
