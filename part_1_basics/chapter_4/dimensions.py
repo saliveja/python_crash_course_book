a3a6399a6399255943f014812e401748fa74b8f5
saliveja <esman@riseup.net>
2022-02-06 10:07:02 +0100

    dimensions modifying the tuple

--- part_1_basics/chapter_4/dimensions.py
+++ part_1_basics/chapter_4/dimensions.py
@@ -1,6 +1,6 @@
 
-#tuple
-#values that cannot change are called immutable
+# tuple
+# values that cannot change are called immutable
 # an immutable list is called a tuple
 
 
@@ -14,3 +14,21 @@ dimensions[0] = 250
 print(dimensions[0])
 # the value of index 0 cannot be change,
 # when it is defined in the first variable
+# if we want immutable values for the rectangle;
+# it is good that python communicates and error
+
+dimensions = (200,50)
+for dimension in dimensions:
+	print(dimension)
+
+# writing over a tuple
+dimensions = (200,50)
+print("Original dimensions:")
+for dimension in dimensions:
+	print(dimension)
+
+dimensions = (400,100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+	print(dimension)
+	# we can modify the tuple by writing a new variable
--- part_1_basics/chapter_4/try_it_yourself_4_12_more_loops.py
+++ part_1_basics/chapter_4/try_it_yourself_4_12_more_loops.py
@@ -15,3 +15,13 @@ print("\nMy friend's favorite foods are:")
 for friend_food in friend_foods:
 	print(friend_food.title())
 
+# writing over a tuple
+dimensions = (200,50)
+print("Original dimensions:")
+for dimension in dimensions:
+	print(dimensions)
+
+dimensions = (400,100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+	print(dimensions)
