5ff4b504d4c26b492a74b0b8f704fcadbdb40f79
saliveja <esman@riseup.net>
2022-02-06 11:24:07 +0100

    try it yourself slices organized with pep standards

--- part_1_basics/chapter_4/try_it_yourself_4_10_slices.py
+++ part_1_basics/chapter_4/try_it_yourself_4_10_slices.py
@@ -1,5 +1,6 @@
 
-items = ['hairbrush', 'tshirt', 'pants', 'socks', 'toothbrush', 'sweater', 'hoodie', 'glasses', 'shoes', 'computer', 'camera']
+items = ['hairbrush', 'tshirt', 'pants', 'socks', 'toothbrush', 'sweater', \
+'hoodie', 'glasses', 'shoes', 'computer', 'camera']
 print("The first three items in the list are:")
 for item in items[:3]:
 	print(item.title())
