6be7c24d5278f2041a3b0f5cc719aba23c4d0ed0
saliveja <esman@riseup.net>
2022-02-06 10:23:02 +0100

    buffet creating tuple with new values

--- part_1_basics/chapter_4/try_it_yourself_4_13_buffet.py
+++ part_1_basics/chapter_4/try_it_yourself_4_13_buffet.py
@@ -4,4 +4,9 @@ buffets = ('pasta', 'soup', 'pizza', 'steak', 'wok')
 for buffet in buffets:
 	print(buffet)
 # when creating a tuple with name of items we need 'x'
-# when using numbers this is not needed
+# when using numbers '' this is not needed
+
+buffets = ('pasta', 'dumpling', 'soup', 'sushi', 'wok')
+for buffet in buffets:
+	print(buffet)
+# this creates a tuplewith new values which overwrites the previous
