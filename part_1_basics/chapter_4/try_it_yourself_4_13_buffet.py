f938643e6e685e0dc5f3e0225897476442976c66
saliveja <esman@riseup.net>
2022-02-06 11:04:18 +0100

    python notes update with PEP8 styling guide

--- part_1_basics/chapter_4/try_it_yourself_4_13_buffet.py
+++ part_1_basics/chapter_4/try_it_yourself_4_13_buffet.py
@@ -3,10 +3,14 @@
 buffets = ('pasta', 'soup', 'pizza', 'steak', 'wok')
 for buffet in buffets:
 	print(buffet)
+print("\n\n")
 # when creating a tuple with name of items we need 'x'
 # when using numbers '' this is not needed
 
-buffets = ('pasta', 'dumpling', 'soup', 'sushi', 'wok')
+buffets = ('pasta', 'dumplings', 'soup', 'sushi', 'wok')
 for buffet in buffets:
 	print(buffet)
 # this creates a tuplewith new values which overwrites the previous
+# the name of the tuple is the same as the first
+
+
