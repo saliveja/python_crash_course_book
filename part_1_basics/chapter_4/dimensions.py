a7316bafd4171ebdc98f07e5795d094bb12c938a
saliveja <esman@riseup.net>
2022-02-06 09:38:22 +0100

    dimensions creating a rectangle with tuple

--- part_1_basics/chapter_4/dimensions.py
+++ part_1_basics/chapter_4/dimensions.py
@@ -2,3 +2,15 @@
 #tuple
 #values that cannot change are called immutable
 # an immutable list is called a tuple
+
+
+dimensions = (200, 50)
+print(dimensions[0])
+#printing value in first position
+print(dimensions[1])
+# printing value in the second position
+
+dimensions[0] = 250
+print(dimensions[0])
+# the value of index 0 cannot be change,
+# when it is defined in the first variable
