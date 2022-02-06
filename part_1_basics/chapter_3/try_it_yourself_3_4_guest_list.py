37d3e38fe53fc074f0084340aa04fbdcb2fd3260
saliveja <esman@riseup.net>
2022-02-06 11:15:51 +0100

    try it yourself guest list organized to pep standards

--- part_1_basics/chapter_3/try_it_yourself_3_4_guest_list.py
+++ part_1_basics/chapter_3/try_it_yourself_3_4_guest_list.py
@@ -1,31 +1,43 @@
 
-guests = ['Kerstin' , 'Erika' , 'mother' , 'beloved comrade' , 'Marie' , 'Birgitta']
+guests = ['Kerstin' , 'Erika' , 'mother' , 'beloved comrade' , 'Marie' , \
+'Birgitta']
 print(guests)
 # guest list before changes
 
 guests.append('Sandra')
 print(guests)
-# adding guest at the end of the list.
+# adding guest at the end of the list
 
 guests.insert(1, 'Elsa')
 print(guests)
-# inserting guest at position 1 in the list.
+# inserting guest at position 1 in the list
 
-print("Dear " + guests[1] + ", I'm putting together a dinner on Sunday and hope you'll have time to come. " + guests[0] + " will also come and I know you wanted to se each other for some time. Hope to see you then <3" + "\n") 
+print("Dear " + guests[1] + ", I'm putting together a dinner on Sunday and \
+hope you'll have time to come. " + guests[0] + " will also come and I know \
+you wanted to se each other for some time. Hope to see you then <3" + "\n") 
 
-message = "you are welcome for dinner on Sunday at our place at 1900. If it's a nice day we'll be outside. Send a text so we know if you can come or not. Greetings"
+message = "you are welcome for dinner on Sunday at our place at 1900. If it's \
+a nice day we'll be outside. Send a text so we know if you can come or not. \
+Greetings"
 print("Dear " + guests[0].title() + ", " + message + "\n")
 
-message = "you are welcome for dinner on Sunday at our place at 1900. If it's a nice day we'll be outside. Send a text so we know if you can come or not. Greetings"
+message = "you are welcome for dinner on Sunday at our place at 1900. If it's \
+a nice day we'll be outside. Send a text so we know if you can come or not. \
+Greetings"
 print("My sweet " + guests[3] + ", " + message + "\n")
 
-message = "you are welcome for dinner on Sunday at our place at 1900. If it's a nice day we'll be outside. Send a text so we know if you can come or not. Greetings"
+message = "you are welcome for dinner on Sunday at our place at 1900. If it's \
+a nice day we'll be outside. Send a text so we know if you can come or not. \
+Greetings"
 print("Dear " + guests[4] + " and " + guests[5] + ", " + message)
-# writing in plural even if it's a list can create unnecessary syntax errors, ie. writing guest instead of guests.
+# writing in plural even if it's a list can create unnecessary syntax errors
+# for example writing guest instead of guests
 
-guests = ['Kerstin' , 'Elsa' , 'Erika' , 'mother' , 'beloved comrade' , 'Marie' , 'Birgitta' , 'Sandra']
+guests = ['Kerstin' , 'Elsa' , 'Erika' , 'mother' , 'beloved comrade' , \
+'Marie' , 'Birgitta' , 'Sandra']
 print(guests)
 comrade_not_coming_1 = guests.pop(4)
 print(guests)
-print("Our " + comrade_not_coming_1 + ", Jiyan, will unfortunately not be able to come due to travels.")
-# 'beloved comrade' is not able to come, removed from the list and other guests are informed
+print("Our " + comrade_not_coming_1 + ", Jiyan, will unfortunately not be \
+able to come due to travels.")
+# 'beloved comrade' removed from the list and other guests are informed
