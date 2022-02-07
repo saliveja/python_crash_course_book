5cab0233bc2d8b95ab25a26fabbbe2d7e84b6073
saliveja <esman@riseup.net>
2022-02-07 10:30:33 +0100

    magic numbers mathematical comparison

--- part_1_basics/chapter_5/magic_number.py
+++ part_1_basics/chapter_5/magic_number.py
@@ -4,3 +4,12 @@ if answer != 42:
 # if the number is not 42, print message
 
 age = 19
+# defining that the age is 19
+age < 21
+# asking if the number is less than 21
+age <= 21
+# asking if the number is smaller or equals 21
+age > 21
+# asking if the number is higher than 21
+age >= 21
+# asking if the number is highers than or equals 21
