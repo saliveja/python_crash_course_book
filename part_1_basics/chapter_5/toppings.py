a0181f389dc16ab71ffb4093d523eed3313bc8e3
saliveja <esman@riseup.net>
2022-02-07 10:05:51 +0100

    toppings if request is not print message

---
+++ part_1_basics/chapter_5/toppings.py
@@ -0,0 +1,6 @@
+requested_topping = 'mushrooms'
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
+# the requested topping is mushrooms
+# if the requested topping is not anchovies, print 'Hold the anchovies!'
+# != mean NOT
