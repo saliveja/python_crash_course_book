a6c8d7dba8923d53661260c1daa8884e793990f8
saliveja <esman@riseup.net>
2022-02-07 13:33:30 +0100

    try it yourself 5-1 lower and upper case modification in true and false statements

--- part_1_basics/chapter_5/try_it_yourself_5_1_conditional_tests.py
+++ part_1_basics/chapter_5/try_it_yourself_5_1_conditional_tests.py
@@ -23,5 +23,19 @@ number_2 = 44
 number_3 = 23
 number_4 = 22
 number_1 >= 50 and number_4 >= 20
-
+# both variables need to meet the conditions, but only number_4 do
 number_1 >= 50 or number_4 >= 20
+# number_1 don't meet the conditions but number_4 do which is enough
+# only one of them need to meet the conditions
+
+sport = 'boxing'
+sport == 'aikido'
+# first defines sport. second asks is sport is aikido which is false
+sport == 'boxing'
+# is True
+
+place = 'ROME'
+place.lower() == 'rome'
+# this make all letters in variable place lower case letters. It is True
+place.upper() == 'rome'
+# this make all letters in variable place upper case letter. It is False
