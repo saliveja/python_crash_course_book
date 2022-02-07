52d1b4e4fcc60835fae176c6be6a5bf47bec172a
saliveja <esman@riseup.net>
2022-02-07 09:49:25 +0100

    cars ignoring cases when testing equality

--- part_1_basics/chapter_5/cars.py
+++ part_1_basics/chapter_5/cars.py
@@ -3,9 +3,9 @@ cars = ['audio', 'bmw', 'subaru', 'toyota']
 for car in cars:
     if car == 'bmw':
         print(car.upper())
+    # if the car is a bmw it will be printed with upper case letters
     else:
         print(car.title())
-# if the car is a bmw it will be printed with upper case letters
 # if it's any other car, it will be printed with first letter upper case
 # if --> is a conditional test
 
@@ -13,4 +13,10 @@ car = 'bmw'
 car == 'bmw'
 # first car equals bmw
 # second conditional test on if it's a bmw
-# respons is True
+# respons is: True
+
+car = 'Audi'
+car.lower() == 'audi'
+# response: True
+# will make all letters in the statement lower case before testing equality
+# if car is printed it will show 'Audi'
