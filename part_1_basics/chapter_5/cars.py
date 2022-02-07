fb21f210fe380c5f4266e8746d29acfd89d5d65a
saliveja <esman@riseup.net>
2022-02-07 09:13:48 +0100

    cars conditional test

--- part_1_basics/chapter_5/cars.py
+++ part_1_basics/chapter_5/cars.py
@@ -7,3 +7,10 @@ for car in cars:
         print(car.title())
 # if the car is a bmw it will be printed with upper case letters
 # if it's any other car, it will be printed with first letter upper case
+# if --> is a conditional test
+
+car = 'bmw'
+car == 'bmw'
+# first car equals bmw
+# second conditional test on if it's a bmw
+# respons is True
