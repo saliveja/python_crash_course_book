2f99f3b5ed9721bad0a6d3a7026ba046682855f2
saliveja <esman@riseup.net>
2022-02-07 11:51:37 +0100

    try it yourself 5-1 if and else, and, or conditions

---
+++ part_1_basics/chapter_5/try_it_yourself_5_1_conditional_tests.py
@@ -0,0 +1,27 @@
+guests = ['lena', 'anna', 'greta', 'nihad', 'can']
+name = 'nihad'
+if name in guests:
+    print("Hello " + name.title() + "!")
+else:
+    print("Sorry, you are not on the invitation list.")
+
+guests = ['lena', 'anna', 'greta', 'nihad', 'can']
+name = 'bengt'
+if name in guests:
+    print("Hello " + name.title() + "!")
+else:
+    print("Sorry, you are not on the invitation list.")
+
+foods = ['apple', 'cheese', 'soda', 'cream']
+'apple' in foods
+# True
+'meat' in foods
+#  False
+
+number_1 = 32
+number_2 = 44
+number_3 = 23
+number_4 = 22
+number_1 >= 50 and number_4 >= 20
+
+number_1 >= 50 or number_4 >= 20
