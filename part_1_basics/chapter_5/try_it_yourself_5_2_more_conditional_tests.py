d723114700bd973a2c6a4920ffd25632accf8050
saliveja <esman@riseup.net>
2022-02-08 09:18:26 +0100

    try it yourself 5-2 item in the list, item not in the list, numerical questions

--- part_1_basics/chapter_5/try_it_yourself_5_2_more_conditional_tests.py
+++ part_1_basics/chapter_5/try_it_yourself_5_2_more_conditional_tests.py
@@ -7,10 +7,25 @@
 # Numerical test on inequality and equality, greater and lesser than, greater \
 # than and equal to and lesser than or equal to
 
+marie = 40
+danny = 18
+marie <= 21 and danny >= 15
+marie >= 21 and danny >= 15
+marie > 45 or danny < 60
 
 # test using the and keyword and the or keyword
 
 
 # test whether an item is in the list
+colors = ['red', 'black', 'green', 'purple', 'white', 'grey']
+for color in colors:
+    if color == 'black':
+        print(color.upper())
 
 # test whether an item is not in the list
+colors = ['red', 'black', 'green', 'purple', 'white', 'grey']
+color = 'black'
+if color not in colors:
+    print('Oh no!')
+else:
+    (color + " is my favorite color")
