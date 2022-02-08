dc17fe8c6a418be192758e9a7fff9759c3e79a28
saliveja <esman@riseup.net>
2022-02-08 09:01:00 +0100

    try it yourself 5-2 first commit

---
+++ part_1_basics/chapter_5/try_it_yourself_5_2_more_conditional_tests.py
@@ -0,0 +1,16 @@
+# tests for equality and inequality
+
+
+# tests using the lower() function
+
+
+# Numerical test on inequality and equality, greater and lesser than, greater \
+# than and equal to and lesser than or equal to
+
+
+# test using the and keyword and the or keyword
+
+
+# test whether an item is in the list
+
+# test whether an item is not in the list
