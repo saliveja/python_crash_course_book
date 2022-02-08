b2be345ff0c483fb090fd52efc72b55654702404
saliveja <esman@riseup.net>
2022-02-08 09:47:43 +0100

    try it yourself 5-2 notes on exercise

--- part_1_basics/chapter_5/try_it_yourself_5_2_more_conditional_tests.py
+++ part_1_basics/chapter_5/try_it_yourself_5_2_more_conditional_tests.py
@@ -1,5 +1,11 @@
 # tests for equality and inequality
+number = '16'
+number == '25'
+# False
 
+item = 'Book'
+item.lower() == 'book'
+# True
 
 # tests using the lower() function
 clothes = ['tshirt', 'hoodie', 'pants', 'socks']
@@ -8,9 +14,11 @@ if item == 'tshirt':
     print("i would have preferred a hoodie")
 else:
     print("Thank you for packing my " + item.lower())
+# printing "Thank you for oacking my hoodie"
 
 clothes = 'Hoodie'
 clothes.lower() == 'hoodie'
+# True bcs using lower() function
 
 # Numerical test on inequality and equality, greater and lesser than, greater \
 # than and equal to and lesser than or equal to
@@ -19,14 +27,21 @@ clothes.lower() == 'hoodie'
 marie = 40
 danny = 18
 marie <= 21 and danny >= 15
+# conditional that if marie is lesser than or equal to 21 and danny is bigger\
+# than or equal to 15 the statement will be True
 marie >= 21 and danny >= 15
+# marie bigger than or equal to 21 and danny bigger than or equal to 15 the\
+# statement will be True
 marie > 45 or danny < 60
+# marie bigger than 45 or danny smaller than 60 - True
 
 # test whether an item is in the list
 colors = ['red', 'black', 'green', 'purple', 'white', 'grey']
 for color in colors:
     if color == 'black':
         print(color.upper())
+# using for loop. If the color black is in the list, print black in \
+# upper case letters
 
 # test whether an item is not in the list
 colors = ['red', 'black', 'green', 'purple', 'white', 'grey']
@@ -34,4 +49,6 @@ color = 'black'
 if color not in colors:
     print('Oh no!')
 else:
-    (color + " is my favorite color")
+    (color.title() + " is my favorite color")
+# if the color black is not in colors, print "Oh no!" and if it is\
+# print "Black is my favorite color"
