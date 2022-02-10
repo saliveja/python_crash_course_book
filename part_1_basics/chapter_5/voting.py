ad2a0b1d6dadabe25c59f913cb8b8cfbe2597197
saliveja <esman@riseup.net>
2022-02-10 10:25:38 +0100

    voting conditional test, if and else

---
+++ part_1_basics/chapter_5/voting.py
@@ -0,0 +1,11 @@
+age = 19
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18!")
+# with the conditional test, we want to take one action if the test passes,\
+# and a different action in all other cases --> else
+
+
