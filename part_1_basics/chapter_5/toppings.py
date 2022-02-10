d4f067d4dfdcf0459b03f0987df3e2bfd9f669ee
saliveja <esman@riseup.net>
2022-02-10 14:02:46 +0100

    try it yourself 5-3 alien colors 1

--- part_1_basics/chapter_5/toppings.py
+++ part_1_basics/chapter_5/toppings.py
@@ -10,3 +10,6 @@ print("\nFinished making your pizza!")
 # all three are independent tests and evaluated
 # mushrooms andextra cheese are found in the list and are printed
 # print last text message
+# the code wouldn't work with elif since elif stops after a test passes
+# because of this only mushrooms would be added and printed together\
+# with the text message in the end
---
+++ part_1_basics/chapter_5/try_it_yourself_5_3_alien_colors_1.py
@@ -0,0 +1,4 @@
+# test if the aliens color is green
+alien_color = ['green', 'yellow', 'red']
+if 'green' in alien_color:
+    print("You just earned 5 points")
