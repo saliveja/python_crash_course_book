bed2d8479081bac73adf00d9b48a17584ba25b79
saliveja <esman@riseup.net>
2022-02-10 14:26:00 +0100

    try it yourself 5-4 alien colors 2 true and false if statement

--- part_1_basics/chapter_5/try_it_yourself_5_4_alien_colors_2.py
+++ part_1_basics/chapter_5/try_it_yourself_5_4_alien_colors_2.py
@@ -4,6 +4,8 @@ if 'green' in alien_color:
     print("You just earned 5 points")
 else:
     print('You just earned 10 points')
+# if green is in the list printing "You just earned 5 points"
+# else print "You just earned 10 points"
 
 alien_color = ['green', 'yellow', 'red']
 if 'black' in alien_color:
@@ -11,3 +13,4 @@ if 'black' in alien_color:
 else:
     points = 10
 print('You just earned ' + str(points) + '.')
+# test if black is in alien color, else earning 10 points
