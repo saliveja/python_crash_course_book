fbb0b332c1ad161cbf498e137d8498de4e587027
saliveja <esman@riseup.net>
2022-02-10 11:36:18 +0100

    toppings modified from before with testing multiple conditions

--- part_1_basics/chapter_5/toppings.py
+++ part_1_basics/chapter_5/toppings.py
@@ -1,6 +1,12 @@
-requested_topping = 'mushrooms'
-if requested_topping != 'anchovies':
-    print("Hold the anchovies!")
-# the requested topping is mushrooms
-# if the requested topping is not anchovies, print 'Hold the anchovies!'
-# != mean NOT
+# testing multiple conditions
+requested_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+print("\nFinished making your pizza!")
+# all three are independent tests and evaluated
+# mushrooms andextra cheese are found in the list and are printed
+# print last text message
