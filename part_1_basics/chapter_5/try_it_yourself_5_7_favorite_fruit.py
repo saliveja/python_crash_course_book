f562d84671e74b0686b0ca0365b54d32239d36ab
saliveja <esman@riseup.net>
2022-02-10 19:05:05 +0100

    try it yourself 5-7 if statements on items in list

---
+++ part_1_basics/chapter_5/try_it_yourself_5_7_favorite_fruit.py
@@ -0,0 +1,22 @@
+# list of favorite fruits and making if statements abt them
+favorite_fruits = ['mango', 'grapes', 'apple']
+if 'mango' in favorite_fruits:
+    print("I love mango!")
+
+if 'bananas' in favorite_fruits:
+    print("You are the one who want to eat bananas.")
+else:
+    print("Banana is just too sweet for me.")
+
+if 'grapes' in favorite_fruits:
+    print("Oh, happy gej, love grapes.")
+else:
+    print("Next time I'll make sure to I have at home")
+
+if 'pineapple' in favorite_fruits:
+    print("Why not.")
+else:
+    print("I prefer mango anyway")
+
+if 'apple' in favorite_fruits:
+    print("My mothers apple pie is amazing <3")
