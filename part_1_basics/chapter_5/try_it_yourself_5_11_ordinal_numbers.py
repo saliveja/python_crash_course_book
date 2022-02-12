42d7b2b8d8fb13f743768d73d1f9036d3943fb2e
saliveja <esman@riseup.net>
2022-02-12 09:10:50 +0100

    try it yourself 5-11 if-elif-else chain inside for loop

---
+++ part_1_basics/chapter_5/try_it_yourself_5_11_ordinal_numbers.py
@@ -0,0 +1,16 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# ~ print(numbers)
+for number in numbers:
+    if number == 1:
+        # be careful how to write condition
+        # 1 == numbers mean if 1 is in numbers
+        # if 1 in numbers will print 'st' every time it finds number 1
+        # and because it's a loop it will repeat even for other numbers
+        print(str(number) + "st")
+    elif number == 2:
+        print(str(number) + "nd")
+    elif number == 3:
+        print(str(number) + "rd")
+    else:
+        print(str(number) + "th")
