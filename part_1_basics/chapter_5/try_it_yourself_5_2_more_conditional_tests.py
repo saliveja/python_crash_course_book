33e3189b08b3966bbb1f79e724fffa4d701fb568
saliveja <esman@riseup.net>
2022-02-12 10:39:11 +0100

    try it yourself 5-2 updating notes and pep8

--- part_1_basics/chapter_5/try_it_yourself_5_2_more_conditional_tests.py
+++ part_1_basics/chapter_5/try_it_yourself_5_2_more_conditional_tests.py
@@ -14,16 +14,17 @@ if item == 'tshirt':
     print("i would have preferred a hoodie")
 else:
     print("Thank you for packing my " + item.lower())
-# printing "Thank you for packing my hoodie"
-
+    # printing "Thank you for packing my hoodie"
+print("\n")
 clothes = 'Hoodie'
 clothes.lower() == 'hoodie'
-# True bcs using lower() function
+# lower() function changes the letter i variable 'clothes' to lower case
+# without lower() the conditional test would be False
+
 
 # Numerical test on inequality and equality, greater and lesser than, greater \
 # than and equal to and lesser than or equal to
 # test using the and keyword and the or keyword
-
 marie = 40
 danny = 18
 marie <= 21 and danny >= 15
@@ -33,25 +34,25 @@ marie >= 21 and danny >= 15
 # marie bigger than or equal to 21 and danny bigger than or equal to 15 the\
 # statement will be True
 marie > 45 or danny < 60
-# marie bigger than 45 or danny smaller than 60 - True
+# marie bigger than 45 or danny smaller than 60 --> True
 
 # test whether an item is in the list
 colors = ['red', 'black', 'green', 'purple', 'white', 'grey']
 for color in colors:
     if color == 'black':
         print(color.upper())
-# using for loop. If the color black is in the list, print black in \
-# upper case letters
-
+        # using for loop. If the color black is in the list, print black in \
+        # upper case letters
+print("\n")
 # test whether an item is not in the list
 colors = ['red', 'black', 'green', 'purple', 'white', 'grey']
 color = 'black'
 if color not in colors:
     print('Oh no!')
 else:
-    (color.title() + " is my favorite color")
-# if the color black is not in colors, print "Oh no!" and if it is\
-# print "Black is my favorite color"
+    print(color.title() + " is my favorite color")
+    # if the color black is not in colors, print "Oh no!" and if it is\
+    # print "Black is my favorite color"
 
 print("\n\n")
 
