17b47447d611720bf94b09f31d97f402f1572761
saliveja <esman@riseup.net>
2022-02-12 10:50:38 +0100

    magic number updating to pep8

--- part_1_basics/chapter_5/magic_number.py
+++ part_1_basics/chapter_5/magic_number.py
@@ -1,7 +1,7 @@
 answer = 17
 if answer != 42:
     print("That is not the correct answer. Please try again!")
-# if the number is not 42, print message
+    # if the number is not 42, print message
 
 age = 19
 # defining that the age is 19
