f8066aaef09fa74eeaf764cba99a6e080878b447
saliveja <esman@riseup.net>
2022-02-12 12:00:14 +0100

    banned users update to pep8

--- part_1_basics/chapter_5/banned_users.py
+++ part_1_basics/chapter_5/banned_users.py
@@ -3,8 +3,8 @@ banned_users = ['andrew', 'carolina', 'david']
 user = 'marie'
 if user not in banned_users:
     print(user.title() + ", you can post a response if you wish.")
-# if the user is not on the list banned_users then a message will be printed\
-# including the users name
+    # if the user is not on the list banned_users then a message will be \
+    # printed including the users name
 
 banned_users = ['andrew', 'carolina', 'david']
 user = 'andrew'
@@ -12,7 +12,7 @@ if user not in banned_users:
     print(user.title() + ", you can post a response if you wish.")
 else:
     print(" good try!")
-# if the user is on the list on banned_users the else option will be printed
+    # if the user is on the list on banned_users the else option will be printed
 
 # boolean expression (another name for a conditional test)
 game_active = True
