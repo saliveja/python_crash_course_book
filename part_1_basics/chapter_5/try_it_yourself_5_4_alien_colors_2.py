3c7e474689b2461d79c3958c514a01e34516282f
saliveja <esman@riseup.net>
2022-02-12 10:30:49 +0100

    try it yourself 5-4 updating pep8

--- part_1_basics/chapter_5/try_it_yourself_5_4_alien_colors_2.py
+++ part_1_basics/chapter_5/try_it_yourself_5_4_alien_colors_2.py
@@ -4,8 +4,8 @@ if 'green' in alien_color:
     print("You just earned 5 points")
 else:
     print('You just earned 10 points')
-# if green is in the list printing "You just earned 5 points"
-# else print "You just earned 10 points"
+    # if green is in the list printing "You just earned 5 points"
+    # else print "You just earned 10 points"
 
 alien_color = ['green', 'yellow', 'red']
 if 'black' in alien_color:
