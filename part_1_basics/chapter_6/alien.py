8e1c1f1f05b8a6218fb02a34d03758ff891d3c82
saliveja <esman@riseup.net>
2022-02-12 22:14:19 +0100

    alien modifying values

--- part_1_basics/chapter_6/alien.py
+++ part_1_basics/chapter_6/alien.py
@@ -43,3 +43,10 @@ alien_0['points'] = 5
 print(alien_0)
 # works like adding a new key-value pair
 # normally we start with empty dictionaries
+
+# modifying values in a dictionary
+alien_0 = {'color': 'green'}
+print('The alien is now ' + alien_0['color'] + '.')
+
+alien_0['color'] = 'yellow'
+print('The alien is now ' + alien_0['color'] + '.')
