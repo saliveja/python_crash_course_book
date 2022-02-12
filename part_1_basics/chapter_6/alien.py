2ae2989f7d519185d91b4cc52291e9b2a5d65485
saliveja <esman@riseup.net>
2022-02-12 14:11:15 +0100

    alien key-value pairs

--- part_1_basics/chapter_6/alien.py
+++ part_1_basics/chapter_6/alien.py
@@ -1,3 +1,12 @@
-alien_0 = {'color 'green', 'points': 5}
+alien_0 = {'color': 'green', 'points': 5}
+# dictionary storing different values about a particular item
 print(alien_0['color'])
 print(alien_0['points'])
+# printing 'green' and '5'
+# a dictionary wrapps the information in {}
+# a key-value pair is a set of value associated with each other
+# every key is connected to its value by a colon
+# the key-value pairs are separated by commas
+# one key value pair is {'color': 'green'}
+# 'color' is a key and the value is 'green'
+# we can store as many as we want
