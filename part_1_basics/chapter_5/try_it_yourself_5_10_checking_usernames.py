4981d1034cd6fe5bea73f3db86d0f3d39c341e74
saliveja <esman@riseup.net>
2022-02-12 08:36:54 +0100

    try it yourself 5-11 first commit

--- part_1_basics/chapter_5/try_it_yourself_5_10_checking_usernames.py
+++ part_1_basics/chapter_5/try_it_yourself_5_10_checking_usernames.py
@@ -1,2 +1,14 @@
 current_users = ['sara', 'tor', 'greta', 'linnea', 'ben']
 new_users = ['dylan', 'kim', 'athena', 'tor', 'linnea']
+for new_user in new_users:
+    if new_user.lower() in current_users:
+        # this means to check if any if new_users is among all of current users
+        print("The username you have chosen is not available.")
+    else:
+        print("This username is available."
+              " You are registered as "
+              + new_user.title())
+# all users also appearing in the current user list will receive a message
+# that the chosen username is unavailable
+# for those who don't already exist in current user will receive a message\
+# that the username is available and that they are registered
