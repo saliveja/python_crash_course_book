8bb5ab5d45027dfa5c7a7d458c64a882f2e986af
saliveja <esman@riseup.net>
2022-02-12 16:08:28 +0100

    alien assessing values in dictionary,  adding key-value pairs, starting with an empty dictionary

--- part_1_basics/chapter_6/alien.py
+++ part_1_basics/chapter_6/alien.py
@@ -11,3 +11,27 @@ print(alien_0['points'])
 # one key value pair is {'color': 'green'}
 # 'color' is a key and the value is 'green'
 # we can store as many as we want
+
+# starting with an empty directory
+alien_0 = {}
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+# adding values to the dictionary
+print(alien_0)
+# this prints the dictionary as one line:
+# {'color': 'green', 'points': 5}
+
+# accessing values in a dictionary
+print(alien_0['color'])
+# this print the value green only
+new_points = alien_0['points']
+# new points equals alien_0 points which is 5
+print('You just earned ' + str(new_points) + ' points!')
+
+# adding new key-value pair
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+alien_0['x position'] = 0
+alien_0['y position'] = 25
+print(alien_0)
