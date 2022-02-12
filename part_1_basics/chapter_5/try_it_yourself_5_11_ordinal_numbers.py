bc59a49fcf956d1cca587ee4a9497ce8dd664fc6
saliveja <esman@riseup.net>
2022-02-12 09:13:46 +0100

    try it yourself 5-11 adding notes

--- part_1_basics/chapter_5/try_it_yourself_5_11_ordinal_numbers.py
+++ part_1_basics/chapter_5/try_it_yourself_5_11_ordinal_numbers.py
@@ -9,8 +9,12 @@ for number in numbers:
         # and because it's a loop it will repeat even for other numbers
         print(str(number) + "st")
     elif number == 2:
+        # it's also possible to write elif 2 == number:
+        # watch out for if it's singular or plural reference to list
+        # number or numbers
         print(str(number) + "nd")
     elif number == 3:
         print(str(number) + "rd")
     else:
         print(str(number) + "th")
+        # make sure else is in the for loop
