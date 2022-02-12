70b17bc9aeffcddea006a399c8436d7cea2c4efd
saliveja <esman@riseup.net>
2022-02-12 10:26:17 +0100

    try it yourself 5-8 updating notes and pep8

--- part_1_basics/chapter_5/try_it_yourself_5_8_hello_admin.py
+++ part_1_basics/chapter_5/try_it_yourself_5_8_hello_admin.py
@@ -1,9 +1,11 @@
+# making a list with users and printing a greeting to each of them
 users = ['admin', 'tor', 'greta', 'linnea', 'ben']
 for user in users:
     if user == 'admin':
+        # if the user is 'admin'
+        # printing --> 'Hello admin, there is no new updates on the project'
         print("Hello " + user.title() + ", there is no new updates on the"
                                         " project.")
     else:
         print("Hello " + user.title() + ", and welcome!")
-# making a list with users and printing a greeting to each of them
-# for the user admin a special greeting will be printed
+        # for all other users 'Hello user, and welcome! is printed
