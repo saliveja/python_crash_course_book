80ea286fb3a8a5f0bbf9d177237443e470bdf2d0
saliveja <esman@riseup.net>
2022-02-12 10:27:58 +0100

    try it yourself 5-6 updating pep8

--- part_1_basics/chapter_5/try_it_yourself_5-_6_stages_of_life.py
+++ part_1_basics/chapter_5/try_it_yourself_5-_6_stages_of_life.py
@@ -12,5 +12,5 @@ elif age < 65:
     print("You are an adult.")
 else:
     print("You are an elder.")
-# when we have a range, such as between 13 and 20
-# we only have to write the last number and < and the range will be covered
+    # when we have a range, such as between 13 and 20
+    # we only have to write the last number and < and the range will be covered
