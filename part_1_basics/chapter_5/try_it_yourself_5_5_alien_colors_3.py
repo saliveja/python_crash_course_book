51f31cb2010e9e3965e6ae129287e5c279080d9d
saliveja <esman@riseup.net>
2022-02-12 10:30:01 +0100

    try it yourself 5-5 updating pep8

--- part_1_basics/chapter_5/try_it_yourself_5_5_alien_colors_3.py
+++ part_1_basics/chapter_5/try_it_yourself_5_5_alien_colors_3.py
@@ -8,7 +8,7 @@ elif 'red' in alien_color:
     points = 15
 print("You've earned " + str(points) + "points.")
 # using elif to test if 'green', 'yellow' and 'red is in the list.
-# Because it's a chain with elif, it's stops after one stateent is true
+# Because it's a chain with elif, it's stops after one statement is true
 
 if 'green' in alien_color:
     print("You've earned 5 points.")
@@ -16,7 +16,7 @@ if 'yellow' in alien_color:
     print("You've earned 10 points.")
 if 'red' in alien_color:
     print("You've earned 15 points.")
-# all three if statements are tru and all three options are printed
+    # all three if statements are true and all three options are printed
 
 alien_color = 'yellow'
 # changing from list of colors to variable with value 'yellow'
@@ -27,4 +27,4 @@ if 'yellow' in alien_color:
 else:
     points = 15
 print("You've earned " + str(points) + " points.")
-# print "You've earned 10 points.
+# printing "You've earned 10 points."
