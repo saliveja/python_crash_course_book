4f3988fc81372c90a368fd9de26acc7ddeca903a
saliveja <esman@riseup.net>
2022-02-13 09:20:57 +0100

    try it yourself 6-1 creating a dictionary and storing values

---
+++ part_1_basics/chapter_6/try_it_yourself_6_1_person.py
@@ -0,0 +1,8 @@
+person = {}
+person['first_name'] = 'Elin'
+person['last_name'] = 'Andersson'
+person['age'] = 25
+person['city'] = 'Berlin'
+print(person)
+# creating a dictionary storing values
+# first name, last name, age and city and printing
