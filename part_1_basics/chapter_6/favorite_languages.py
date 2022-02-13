936a5266acc25a11d831abf11a372034a3b6c7d1
saliveja <esman@riseup.net>
2022-02-13 14:00:54 +0100

    favorite language using keys()

--- part_1_basics/chapter_6/favorite_languages.py
+++ part_1_basics/chapter_6/favorite_languages.py
@@ -12,3 +12,23 @@ print("Sarah's favorite language is " +
 for name, language in favorite_languages.items():
     print(name.title() + "'s favorite language is " +
           language.title() + ".")
+print("\n")
+# key() method
+# returns  a list of all keys
+for name in favorite_languages.keys():
+    print(name.title())
+    # this for loop is the same as:
+    # for name in favorites_languages:
+print("\n")
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(name.title())
+
+    if name in friends:
+        print("Hi " + name.title() +
+              ", I see you favorite language is " +
+              favorite_languages[name].title() + "!")
+    if 'erin' not in favorite_languages.keys():
+        print("Erin, please take our poll!")
+
+#
