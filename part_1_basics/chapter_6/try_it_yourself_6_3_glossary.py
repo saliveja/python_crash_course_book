25b2ec9d2b350423517147b9fb31038ab7366e9a
saliveja <esman@riseup.net>
2022-02-13 10:17:32 +0100

    try it yourself 6-3 creating glossary

---
+++ part_1_basics/chapter_6/try_it_yourself_6_3_glossary.py
@@ -0,0 +1,17 @@
+glossary = {}
+glossary['dictionary'] = "A dictionary contains key-value pairs, a cathegory" \
+                         " with assigned value for example 'color': 'black."
+glossary['key_value_pair'] = "A key value pair is the stored value in a " \
+                             "dictionary."
+glossary['if_elif_else chain'] = "An if-elif-else cchain is a series of" \
+                                 " conditional tests."
+glossary['lists'] = "A list contains items. Contrary to a dictionary it " \
+                    " doesn't hold any stored value about the items."
+glossary['modifying_value'] = "If we want to modify the value of a " \
+                              " key-value pair in a dictionary we can 'add' " \
+                              " it again but assign new value."
+print("Dictionary: " + str(glossary['dictionary']) + "\n")
+print("Key-value pair: " + str(glossary['key_value_pair']) + "\n")
+print("If-elif-else chain: " + str(glossary['if_elif_else chain']) + "\n")
+print("Lists: " + str(glossary['lists']) + "\n")
+print("Modifying value: " + str(glossary['modifying_value']) + "\n")
