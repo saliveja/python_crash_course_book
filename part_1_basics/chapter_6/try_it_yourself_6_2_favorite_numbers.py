506055ed4e0216a7b47ce9fddaa577c1a6d886b3
saliveja <esman@riseup.net>
2022-02-13 09:38:07 +0100

    try it yourself 6-2 - creating dictionary and printing message with value

---
+++ part_1_basics/chapter_6/try_it_yourself_6_2_favorite_numbers.py
@@ -0,0 +1,9 @@
+favorite_numbers = {'lina': 1921, 'elsa': 8, 'isak': 1, 'carlos': 2012,
+                    'amargi': 27}
+print("Lina's favorite number is " + str(favorite_numbers['lina']))
+print("Elsa's favorite number is " + str(favorite_numbers['elsa']))
+print("Isak's favorite number is " + str(favorite_numbers['isak']))
+print("Carlos favorite number is " + str(favorite_numbers['carlos']))
+print("Amargi's favorite number is " + str(favorite_numbers['amargi']))
+# creating a dictionary with five people and their favorite numbers
+# printing a message including the value
