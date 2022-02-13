77a378018408d7e62d15f2dd13003554cc849d41
saliveja <esman@riseup.net>
2022-02-13 09:13:41 +0100

    favorite languages printing favorite language

---
+++ part_1_basics/chapter_6/favorite_languages.py
@@ -0,0 +1,10 @@
+# making a dictionary with favorite languages
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+print("Sarah's favorite language is " +
+      favorite_languages['sarah'].title() + ".")
+# printing Sarah's favorite language
