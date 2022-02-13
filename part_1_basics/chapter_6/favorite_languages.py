7074b059d91a0b84f0aacd1252b15a3f47babf41
saliveja <esman@riseup.net>
2022-02-13 14:31:22 +0100

    favorite language -am using set() function

--- part_1_basics/chapter_6/favorite_languages.py
+++ part_1_basics/chapter_6/favorite_languages.py
@@ -35,3 +35,19 @@ for name in favorite_languages.keys():
 for name in sorted(favorite_languages.keys()):
     print(name.title() + ", thank you for taking the poll.")
     # with sorted() we can organize keys in alphabetical order
+print("\n")
+
+# looping through all values in a dictionary with values() method
+# this returns a list without keys
+print("The following languages have been mentioned:")
+for language in sorted(favorite_languages.values()):
+    print(language.title())
+    # pulls all values from the list
+    # values() method don't check for repeats
+print("\n")
+
+# using set to not repeat value
+for language in set(favorite_languages.values()):
+    print(language.title())
+    # set() identifies unique items
+    # therefore there is nor repetition of values when printing
