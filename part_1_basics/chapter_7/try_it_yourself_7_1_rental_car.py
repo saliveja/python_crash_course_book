48bd3347d89819df4ce747c5b1f8f6fcd5a88ca1
saliveja <esman@riseup.net>
2022-02-16 10:55:24 +0100

    try it yourself 7-1

---
+++ part_1_basics/chapter_7/try_it_yourself_7_1_rental_car.py
@@ -0,0 +1,6 @@
+prompt = "What kind of car would you like to rent? "
+# asking for input
+car = input(prompt)
+# defining that the car equals what is written in the input
+print(f"Let me see if I can find you a {car.title()}.")
+# printing message including the input
