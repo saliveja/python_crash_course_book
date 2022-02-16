f65f72b59765293a17cc8ede6aa05309902010e9
saliveja <esman@riseup.net>
2022-02-16 14:00:46 +0100

    counting printing as long as condition is met- using while

--- part_1_basics/chapter_7/counting.py
+++ part_1_basics/chapter_7/counting.py
@@ -1,1 +1,11 @@
 # while loop
+
+current_number = 1
+# assigning current number the value 1
+while current_number <= 5:
+    # as long as the current number is less than or equal to 5
+    print(current_number)
+    # printing the current number, which the first time is 1
+    current_number += 1
+    # adding 1 to the value of the current number, which then become two etc
+    # the loop stops running when it reaches 5
