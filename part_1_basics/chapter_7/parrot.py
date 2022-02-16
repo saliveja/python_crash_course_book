61e690a9c064397b6a0bc54ec570bd465f896397
saliveja <esman@riseup.net>
2022-02-16 09:39:24 +0100

    parrot using input()

---
+++ part_1_basics/chapter_7/parrot.py
@@ -0,0 +1,5 @@
+message = input("Tell me something,and I will repeat it back to you: ")
+print(message)
+# the user is asked to enter information which will then be repeated back
+# the text edition won't be able to run this program
+# we need to use terminal
