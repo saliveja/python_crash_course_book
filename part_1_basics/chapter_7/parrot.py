a269e659c4f11c259962bb3dbbac0a3db1ea1f04
saliveja <esman@riseup.net>
2022-02-16 15:10:12 +0100

    parrot using flag, while statment checks True or False condition

--- part_1_basics/chapter_7/parrot.py
+++ part_1_basics/chapter_7/parrot.py
@@ -1,5 +1,35 @@
-message = input("Tell me something,and I will repeat it back to you: ")
+# repeating a message using input()
+message = input("Tell me something,and I will repeat it back to you ")
 print(message)
 # the user is asked to enter information which will then be repeated back
 # the text edition won't be able to run this program
 # we need to use terminal
+
+prompt = "Tell me something,and I will repeat it back to you --> "
+# request to make an input
+prompt += "Enter quit to end the program: "
+# if the input is quit. the program will stop running
+message = ""
+while message != 'quit':
+    message = input(prompt)
+
+    if message != 'quit':
+        print(message)
+
+# using a Flag
+# flag acts as a signal for the program
+# flag is set to True
+# while statements only need to check one condition:
+# if the status of the Flag is True or False
+prompt = "Tell me something,and I will repeat it back to you --> "
+prompt += "Enter quit to end the program: "
+
+active = True
+while active:
+    message = input(prompt)
+
+    if message == 'quit':
+        active = False
+
+    else:
+        print(message)
--- part_1_basics/chapter_7/try_it_yourself_7_3_multiples_of_ten.py
+++ part_1_basics/chapter_7/try_it_yourself_7_3_multiples_of_ten.py
@@ -2,7 +2,11 @@ info = int(input("Can you give me a number and I will say if it's a multiple" \
                  " of ten or not. Which number do you choose?  "))
 
 # that the string should be interpreter as numerical
-if info % 10 == 0:
+if info == 0:
+    print("What are you doing?")
+    # # if the chosen number is 0 it will cause error unless accounting for it
+    # this message will be printed if number 0 is inserted
+elif info % 10 == 0:
     # multiple of ten means that it is an even number and will return 0 if True
     print("\nThe number you have chosen is muliple by ten.")
 else:
