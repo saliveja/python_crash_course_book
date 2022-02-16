7467f737aac4e3bed93f0c14a96d16765215e3eb
saliveja <esman@riseup.net>
2022-02-16 10:11:56 +0100

    greeter multiline string and using int()

---
+++ part_1_basics/chapter_7/greeter.py
@@ -0,0 +1,26 @@
+name = input("Please enter your name: ")
+print(f"\nHello, {name}!")
+# input() makes the request and then waits for input
+# when entering name, the greeting and name is printed
+# the name in print refers to the answer
+
+prompt = "If you tell us who your are, we can personalize the messages you see."
+prompt += "\nWhat is your first name? "
+name = input(prompt)
+print(f"\nHello, {name}!")
+# building a multi-line string
+# the first one is variable
+# the second using += is adding a string to the first string called 'prompt'
+# this will be printed on a new line
+
+# using int()
+age = input("How old are you? ")
+# if anser ie. with 15
+# writing age, terminal will return '15'
+# if writing age >= 18 there will be an error
+# it's nott possible to compare a string with a numerical value
+# if we write age == 15 it will return False
+age = int(age)
+age >= 15
+# returns True because the string is converted to a numerical representation
+# the comparison is therefore possible
