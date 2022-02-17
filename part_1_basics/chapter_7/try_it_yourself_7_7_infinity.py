29eed1599df2d3d1b22f859731d95d71e34055bd
saliveja <esman@riseup.net>
2022-02-17 13:40:27 +0100

    try it yourself 7-7 indefinite loop

---
+++ part_1_basics/chapter_7/try_it_yourself_7_7_infinity.py
@@ -0,0 +1,7 @@
+name = 'anna'
+# defining variable
+while name == 'anna':
+    # while the name anna is the variable
+    # 'anna' will be printed
+    # this creates an indefinite loop
+    print(name)
