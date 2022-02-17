2e829d0c49c054be44f7a73b012184716de60f8b
saliveja <esman@riseup.net>
2022-02-17 10:56:04 +0100

    input() without loop, different responses

--- part_1_basics/chapter_7/try_it_yourself_7_5_movie_tickets.py
+++ part_1_basics/chapter_7/try_it_yourself_7_5_movie_tickets.py
@@ -1,9 +1,15 @@
 question = "How old are you? "
+# because we just need one answer, we don't need to make a loop
 
 answer = input(question)
-if answer < 3:
-    print("Your ticket is free!")
-elif answer < 12: \
-        print("Your ticket costs $10")
+if int(answer) < 3:
+    # if the anser is less than 3
+    print("Welcome. Your ticket is free!")
+    # this message is printed
+elif int(answer) < 12:
+    # if the answer is less than 12
+    print("Welcome! Your ticket cost $10")
+    # this answer is printed
 else:
-    print("Your ticket costs $15")
+    print("Welcome! Your ticket cost $15")
+    # if it's any other number, this message is printed
