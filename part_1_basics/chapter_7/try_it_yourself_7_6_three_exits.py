09bbd675472491593beb60793fbba839e1fffda4
saliveja <esman@riseup.net>
2022-02-18 13:58:27 +0100

    try it yourself 7-6 three exists making code more efficient

--- part_1_basics/chapter_7/try_it_yourself_7_6_three_exits.py
+++ part_1_basics/chapter_7/try_it_yourself_7_6_three_exits.py
@@ -1,39 +1,50 @@
 # using conditions - active, while and if
 
 question = "How old are you? "
-active = True
-
-while active:
-    answer = input(question)
-    if answer == 'quit':
-        # the answer is 'quit'
-        print("Hope to see you soon!")
-        # this message will be printed
-        active = False
-        # the condition is no longer true
-
-    elif int(answer) < 3:
-        print("Welcome. Your ticket is free!")
-        # if the input is less than 3
-    elif int(answer) < 12:
-        print("Welcome! Your ticket cost $10")
-        # if the input is less than 12
-    elif int(answer) > 12:
-        print("Welcome! Your ticket cost $15")
-        # if the input is more than 12
+answer_input = "Welcome, your ticket cost $"
+price_1 = str('0')
+price_2 = str('10')
+price_3 = str('15')
+#
+# active = True
+# while active:
+#     answer = input(question)
+#     if answer == 'quit':
+#         # the answer is 'quit'
+#         print("Hope to see you soon!")
+#         # this message will be printed
+#         active = False
+#         # the condition is no longer true
+#
+#     elif int(answer) < 3:
+#         print(answer_input + price_1)
+#         # if the input is less than 3
+#     elif int(answer) < 12:
+#         print(answer_input + price_2)
+#         # if the input is less than 12
+#     elif int(answer) > 11:
+#         print(answer_input + price_3)
+#         # if the input is more than 12
 
 # using break instead of active
-question = "How old are you? "
-
+# question = "How old are you? "
+stop = 'quit'
 while True:
     answer = input(question)
-    if answer == 'quit':
+    if answer.lower() == stop:
         print("Hope to see you soon!")
         break
 
     elif int(answer) < 3:
-        print("Welcome. Your ticket is free!")
-    elif int(answer) < 12:
-        print("Welcome! Your ticket cost $10")
-    elif int(answer) > 12:
-        print("Welcome! Your ticket cost $15")
+        print(answer_input + price_1)
+    elif int(answer) <= 12:
+        print(answer_input + price_2)
+    elif int(answer) < 110:
+        print(answer_input + price_3)
+    elif int(answer) >= 110:
+        print("Serinko you are the only one testing this")
+    # elif answer != int(answer):
+    #     print("This is not an age, enter a digit, you degen.")
+    # how to make sure that letters are not accepted except quit?
+
+
