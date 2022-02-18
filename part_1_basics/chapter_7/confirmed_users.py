2b0751b8814ecad1e41bd2a62f425ae1113270ce
saliveja <esman@riseup.net>
2022-02-18 09:12:22 +0100

    confirmed users using sort() to organize list alphabetically

--- part_1_basics/chapter_7/confirmed_users.py
+++ part_1_basics/chapter_7/confirmed_users.py
@@ -8,8 +8,17 @@ while unconfirmed_users:
     current_user = unconfirmed_users.pop()
     # with pop() we are able to remove from the list of unconfirmed users
     # and still be able to work with the item
+    # pop() removes from the end of the list
 
     print(f"Verifying user: {current_user.title()}")
     # the current user is the one that was removed from unconfirmed users
     confirmed_users.append(current_user)
     # adding to the list of current users
+
+confirmed_users.sort()
+# organizing the list in alphabetical order
+print("\nThe following users have been confirmed:")
+for confirmed_user in confirmed_users:
+    print(confirmed_user.title())
+    # printing the users that have just been moved to the list of \
+    # confirmed users
