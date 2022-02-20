193ed78a24bf10ad3c904f078b3af0f2acdab29e
saliveja <esman@riseup.net>
2022-02-20 12:52:01 +0100

    try it yourself 8-5 default value and changing arguments adding notes

--- part_1_basics/chapter_8/try_it_yourself_8_5_cities.py
+++ part_1_basics/chapter_8/try_it_yourself_8_5_cities.py
@@ -1,7 +1,11 @@
 def describe_cities(city, country='Greece'):
+    # defining a function with a default value
     print(f"{city.title()} is in {country.title()}.")
+    # within the function printing a message about the values
 
 
 describe_cities('thessaloniki')
 describe_cities('athens')
 describe_cities('paris', country='France')
+# the last time we call for the function we change the value of country
+# the value is instead 'France'
