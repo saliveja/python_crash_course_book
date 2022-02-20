f450ad1bbf588b3599d3f4989bee77d617c19f10
saliveja <esman@riseup.net>
2022-02-20 11:55:15 +0100

    try it yourself 8-3 defining function using positional arguments

---
+++ part_1_basics/chapter_8/try_it_yourself_8_3_tshirt.py
@@ -0,0 +1,6 @@
+def make_tshirt(size, text):
+    print(f"My tshirt is size {size} with the text {text}.")
+
+
+make_tshirt('small', "'Hello Python world!'")
+make_tshirt(text="'Hello Python world!")
