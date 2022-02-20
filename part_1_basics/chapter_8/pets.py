2fdff4144e74e63601a8033bea455b834ea6849b
saliveja <esman@riseup.net>
2022-02-20 10:55:02 +0100

    pets - positional arguments and multiple function call

---
+++ part_1_basics/chapter_8/pets.py
@@ -0,0 +1,18 @@
+# positional arguments
+# is when parameters and the arguments/values are in the same order
+def describe_pet(animal_type, pet_name):
+    # making a function which contains two parameters
+    """Display information about a pet."""
+    print((f"\nI have a {animal_type}."))
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+
+describe_pet('hamster', 'harry')
+# calling for function and giving the values that connects with the parameters
+# animal_type = 'hamster
+# pet_name = 'harry'
+
+# multiple functions call
+describe_pet('dog', 'willie')
+# we can change the value and the function work the same
+# but the new values will be printed instead
