8d2a69f247c6d943db44de579c781792ebada84f
saliveja <esman@riseup.net>
2022-02-20 13:04:25 +0100

    formatted name - addint notes

--- part_1_basics/chapter_8/formatted_name.py
+++ part_1_basics/chapter_8/formatted_name.py
@@ -1,8 +1,12 @@
 def get_formatted_name(first_name, last_name):
+    # defining function, including parameters
     """Return a full name neatly formatted"""
     full_name = (f"{first_name} {last_name}")
+    # making variable that combines the two parameters
     return full_name.title()
+    # returning full name starting with upper case letter
 
 
 musician = get_formatted_name('jimi', 'hendrix')
+# creating a variable which equals calling the function and including values
 print(musician)
