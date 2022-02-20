7681e6e5bacd4af2b1ba81c3c340c42b040f5a71
saliveja <esman@riseup.net>
2022-02-20 11:57:47 +0100

    try it yourself 8-3 defining function using keyword  arguments and default value

--- part_1_basics/chapter_8/try_it_yourself_8_3_tshirt.py
+++ part_1_basics/chapter_8/try_it_yourself_8_3_tshirt.py
@@ -1,6 +1,7 @@
-def make_tshirt(size, text):
+def make_tshirt(text, size='small'):
     print(f"My tshirt is size {size} with the text {text}.")
 
 
 make_tshirt('small', "'Hello Python world!'")
+print("\n**")
 make_tshirt(text="'Hello Python world!")
