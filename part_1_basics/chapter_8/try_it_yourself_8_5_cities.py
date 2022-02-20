3c66cdd9c9a463cf73ba1a2d7c151d8ca7f0899e
saliveja <esman@riseup.net>
2022-02-20 12:15:28 +0100

    try it yourself 8-5 default value and changing arguments

---
+++ part_1_basics/chapter_8/try_it_yourself_8_5_cities.py
@@ -0,0 +1,7 @@
+def describe_cities(city, country='Greece'):
+    print(f"{city.title()} is in {country.title()}.")
+
+
+describe_cities('thessaloniki')
+describe_cities('athens')
+describe_cities('paris', country='France')
