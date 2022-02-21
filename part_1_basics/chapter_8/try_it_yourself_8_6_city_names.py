1859bd9599894ec6e2540e11e732b05e28eb54cf
saliveja <esman@riseup.net>
2022-02-21 09:25:59 +0100

    try it yourself 8-6 three ways of calling a function - positional argument, default value and defining in argument

--- part_1_basics/chapter_8/try_it_yourself_8_6_city_names.py
+++ part_1_basics/chapter_8/try_it_yourself_8_6_city_names.py
@@ -1,7 +1,30 @@
 def city_country(city, country):
+    """Three ways of calling a function"""
     city_and_country = (f'"{city}, {country}"')
     return city_and_country.title()
 
 
 info = city_country('oslo', 'norway')
+# positional argument
 print(info)
+
+
+def city_country(city, country='norway'):
+    # country is default value
+    city_and_country = (f'"{city}, {country}"')
+    return city_and_country.title()
+
+
+info = city_country('oslo')
+print(info)
+
+
+def city_country(city, country):
+    """Printing city and country with for loop"""
+    info = (f'"{city}, {country}"')
+    return info.title()
+
+
+city_and_country = city_country(city='oslo', country='norway')
+# defining both city and country in arguments
+print(city_and_country)
