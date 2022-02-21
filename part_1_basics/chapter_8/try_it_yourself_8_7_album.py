64293b4d54b2ccd4ef58d7b96afd43e2332f4182
saliveja <esman@riseup.net>
2022-02-21 12:42:20 +0100

    try it yourself 8-7 making dictionaries, storing in list, for loop, while, input - trying to store input in textfile

--- part_1_basics/chapter_8/try_it_yourself_8_7_album.py
+++ part_1_basics/chapter_8/try_it_yourself_8_7_album.py
@@ -1,3 +1,5 @@
+# making three dictionaries
+
 laleh = {'Laleh': "'Colors'", }
 
 dead_prez = {'Dead prez': "'Lets get free'", }
@@ -5,7 +7,7 @@ dead_prez = {'Dead prez': "'Lets get free'", }
 tracy_chapman = {'Tracy Chapman': "'Crossroads'", }
 
 albums = []
-
+# adding them to a list
 albums.append(laleh)
 albums.append(dead_prez)
 albums.append(tracy_chapman)
@@ -18,4 +20,45 @@ def make_album():
             print(f"{key.title()}, {value.title()}")
 
 
+# for loop to print each artist and album
+print("\n\n")
+
+# creating a function with while statement
+# attempting not save input info to a text file - NOT working
 make_album()
+info = "Press q if you want to exit."
+question_1 = 'Artist: '
+question_2 = 'Album: '
+question_3 = 'Tracks: '
+
+while True:
+    print(info)
+    answer = input(question_1)
+    if answer == 'q':
+        quit()
+
+    else:
+        store = open("test.txt", "w")
+        store.writelines(answer)
+        # store_1 = close('~/Desktop/test2.odt')
+        print("\n")
+
+        print(info)
+        answer_2 = input(question_2)
+        if answer_2 == 'q':
+            quit()
+
+        else:
+            store.writelines(answer_2)
+            print("\n")
+
+            print(info)
+            answer_3 = input(question_3)
+            if answer_3 == 'q':
+                quit()
+
+            else:
+                store.writelines(answer_3)
+                print("Thank you for your participation.\n")
+
+print("Have a nice day!")
