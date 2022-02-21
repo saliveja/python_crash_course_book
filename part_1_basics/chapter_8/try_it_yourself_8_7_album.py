f77a70e2aadcdcab453d9270474867dab461832e
saliveja <esman@riseup.net>
2022-02-21 13:11:04 +0100

    try it yourself 8-7- album adding track to dictionaries

--- part_1_basics/chapter_8/try_it_yourself_8_7_album.py
+++ part_1_basics/chapter_8/try_it_yourself_8_7_album.py
@@ -1,10 +1,14 @@
 # making three dictionaries
 
-laleh = {'Laleh': "'Colors'", }
+laleh = {'Laleh': "'Colors,'", }
 
-dead_prez = {'Dead prez': "'Lets get free'", }
+dead_prez = {'Dead prez': "'Lets get free,'", }
 
-tracy_chapman = {'Tracy Chapman': "'Crossroads'", }
+tracy_chapman = {'Tracy Chapman': "'Crossroads,'", }
+
+laleh['tracks'] = '15'
+dead_prez['tracks'] = '20'
+tracy_chapman['tracks'] = '12'
 
 albums = []
 # adding them to a list
@@ -14,51 +18,17 @@ albums.append(tracy_chapman)
 
 
 def make_album():
-    """Three dictionaries with albums"""
+    """Three dictionaries"""
+
     for album in albums:
-        for key, value in album.items():
-            print(f"{key.title()}, {value.title()}")
-
-
-# for loop to print each artist and album
-print("\n\n")
-
-# creating a function with while statement
-# attempting not save input info to a text file - NOT working
-make_album()
-info = "Press q if you want to exit."
-question_1 = 'Artist: '
-question_2 = 'Album: '
-question_3 = 'Tracks: '
-
-while True:
-    print(info)
-    answer = input(question_1)
-    if answer == 'q':
-        quit()
-
-    else:
-        store = open("test.txt", "w")
-        store.writelines(answer)
-        # store_1 = close('~/Desktop/test2.odt')
         print("\n")
 
-        print(info)
-        answer_2 = input(question_2)
-        if answer_2 == 'q':
-            quit()
+        for key, value in album.items():
+            # album = ", ".join(album)
+            string = f"{key.title()}, {value.title()}"
+            print(string, end=' ')
 
-        else:
-            store.writelines(answer_2)
-            print("\n")
 
-            print(info)
-            answer_3 = input(question_3)
-            if answer_3 == 'q':
-                quit()
-
-            else:
-                store.writelines(answer_3)
-                print("Thank you for your participation.\n")
-
-print("Have a nice day!")
+make_album()
+# for loop to print each artist and album
+print("\n\n")
