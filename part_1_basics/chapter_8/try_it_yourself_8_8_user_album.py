f53d64f06ad6c6d4f88f383716217507b62c66ff
saliveja <esman@riseup.net>
2022-02-21 13:13:08 +0100

    try it yourself 8-8 function including while, input, attempt to store info from input in file and quit

---
+++ part_1_basics/chapter_8/try_it_yourself_8_8_user_album.py
@@ -0,0 +1,39 @@
+# creating a function with while statement
+# attempting not save input info to a text file - NOT working
+make_album()
+info = "Press q if you want to exit."
+question_1 = 'Artist: '
+question_2 = 'Album: '
+question_3 = 'Tracks: '
+
+while True:
+    print(info)
+    answer = input(question_1)
+    if answer == 'q':
+        quit()
+
+    else:
+        store = open("test.txt", "w")
+        store.writelines(answer)
+        # store_1 = close('~/Desktop/test2.odt')
+        print("\n")
+
+        print(info)
+        answer_2 = input(question_2)
+        if answer_2 == 'q':
+            quit()
+
+        else:
+            store.writelines(answer_2)
+            print("\n")
+
+            print(info)
+            answer_3 = input(question_3)
+            if answer_3 == 'q':
+                quit()
+
+            else:
+                store.writelines(answer_3)
+                print("Thank you for your participation.\n")
+
+print("Have a nice day!")
