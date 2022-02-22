7c7a4258ec2b106bf432b24fcf032fdddec7c218
saliveja <esman@riseup.net>
2022-02-22 10:12:25 +0100

    user profile using arbitrary keyword arguments with dictionaries

---
+++ part_1_basics/chapter_8/user_profile.py
@@ -0,0 +1,22 @@
+def build_profile(first, last, **user_info):
+    # creating parameters which inlcude two positional arguments
+    # and one arbitraty. **user_info creates an empty dictionary
+    # where it store any given key-value pair
+    """Build a dictionary containing everything we know about a user."""
+    profile = {}
+    profile['first_name'] = first
+    # adding key-value pairs to an empty dictionary
+    # first_name equals the first argument
+    profile['last_name'] = last
+    # last_name equals the second argument
+    for key, value in user_info.items():
+        # looping through the dictionary user_info
+        profile[key] = value
+        return profile
+
+
+user_profile = build_profile('albert', 'einstein',
+                             location='princeton',
+                             field='physics')
+                            
+print(user_profile)
