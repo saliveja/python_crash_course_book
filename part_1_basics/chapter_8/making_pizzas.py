bb2a0fd80e3ce272d13f5ca3e06a24ff09eae9c4
saliveja <esman@riseup.net>
2022-02-22 11:06:58 +0100

    making pizzas importing function

---
+++ part_1_basics/chapter_8/making_pizzas.py
@@ -0,0 +1,8 @@
+import pizza
+
+# imports the function in pizza.py
+# if we keep the function without situation specifics,
+# it can be applied in different situations
+
+pizza.make_pizza(16, 'pepperoni')
+pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
--- part_1_basics/chapter_8/pizza.py
+++ part_1_basics/chapter_8/pizza.py
@@ -1,27 +1,27 @@
-# passing an arbitrary number of arguments
-def make_pizza(*toppings):
-    # * creates an empty tuple and includes whatever value it receives
-    """Print the list of toppings that have been requested."""
-    print(toppings)
-
-
-make_pizza('pepperoni')
-# first time we call the function, we added one topping
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
-
-
-# second time we call the function we added three toppings
-
-
-def make_pizza(*toppings):
-    """Summarize the pizza we are about to make."""
-    print("\nMaking pizza with the following toppings:")
-    for topping in toppings:
-        print(f"- {topping}")
-
-
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
+# # passing an arbitrary number of arguments
+# def make_pizza(*toppings):
+#     # * creates an empty tuple and includes whatever value it receives
+#     """Print the list of toppings that have been requested."""
+#     print(toppings)
+#
+#
+# make_pizza('pepperoni')
+# # first time we call the function, we added one topping
+# make_pizza('mushrooms', 'green peppers', 'extra cheese')
+#
+#
+# # second time we call the function we added three toppings
+#
+#
+# def make_pizza(*toppings):
+#     """Summarize the pizza we are about to make."""
+#     print("\nMaking pizza with the following toppings:")
+#     for topping in toppings:
+#         print(f"- {topping}")
+#
+#
+# make_pizza('pepperoni')
+# make_pizza('mushrooms', 'green peppers', 'extra cheese')
 
 
 # mixing positional and arbitrary arguments
@@ -29,10 +29,10 @@ make_pizza('mushrooms', 'green peppers', 'extra cheese')
 # therefore size goes first of the parameters
 
 def make_pizza(size, *toppings):
-    print(f"\nMaking a {str(size)}-inch pizza with the following toppings:")
+    print(f"\nMaking a {str(size)}-inch pizza "
+          f"with the following toppings:")
     for topping in toppings:
         print(f"- {topping}")
 
-
-make_pizza(16, 'pepperoni')
-make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+# make_pizza(16, 'pepperoni')
+# make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
