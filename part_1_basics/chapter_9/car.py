820d0e181305a6f3131f7b08c4b0c11c73253153
saliveja <esman@riseup.net>
2022-02-23 11:45:12 +0100

    car updated with spaces between lines

--- part_1_basics/chapter_9/car.py
+++ part_1_basics/chapter_9/car.py
@@ -42,21 +42,27 @@ my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
 my_new_car.read_odometer()
 
+print('\n')
 my_new_car.odometer_reading = 23
 my_new_car.read_odometer()
 
+print('\n')
 my_new_car.update_odometer(23)
 my_new_car.read_odometer()
 
+print('\n')
 my_new_car.update_odometer(56)
 my_new_car.read_odometer()
 
+print('\n')
 my_used_car = Car('subaru', 'outback', 2013)
 print(my_used_car.get_descriptive_name())
 
+print('\n')
 my_used_car.update_odometer(23500)
 my_used_car.read_odometer()
 
+print('\n')
 my_used_car.increment_odometer(100)
 # this adds 100 to the updates value which is 23500
 my_used_car.read_odometer()
