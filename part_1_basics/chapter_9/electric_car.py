e5fc9e0cc503942f651f38c0a41492fe1b62d3dd
saliveja <esman@riseup.net>
2022-02-24 10:58:31 +0100

    electric car - class inheritance, calling method from parent class

---
+++ part_1_basics/chapter_9/electric_car.py
@@ -0,0 +1,51 @@
+# the __init__() method for child class
+
+class Car():
+
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Return neatly formatted descriptive name."""
+        long_name = f" {str(self.year)} {self.make} {self.model}"
+        return long_name.title()
+
+    def update_odometer(self, mileage):
+        """Set the odometer to the given value.
+        Reject the change if it attempts to roll the odometer back"""
+
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def read_odometer(self):
+        """print a statement showing the car's mileage."""
+        print(f"This car has {str(self.odometer_reading)} miles on it.")
+
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading"""
+        self.odometer_reading += miles
+
+
+class Electric_car(Car):
+    # in the parenthesis we refer to the parent class Car
+    """Represent aspects of a car, specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        # the information needed to make a car instance
+        """Initialize attribute of the parent class."""
+        super().__init__(make, model, year)
+        # the super().__init__ function makes the connection between the
+        # parent (superclass) and child class (subclass)
+        # and gives the child class all attributes of the parent class
+
+
+my_tesla = Electric_car('tesla', 'model s', 2016)
+# making an instance, storing arguments in variable my_tesla
+print(my_tesla.get_descriptive_name())
+# calling a method in parent class
