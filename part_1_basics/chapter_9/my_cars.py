25cb16f452a5a831e6023506f339b51d2a1ec6bc
saliveja <esman@riseup.net>
2022-02-25 14:06:52 +0100

    notes

--- part_1_basics/chapter_9/my_cars.py
+++ part_1_basics/chapter_9/my_cars.py
@@ -17,7 +17,15 @@ print(my_tesla.get_descriptive_name())
 import car
 
 my_beetle = car.Car('volkswagen', 'beetle', 2016)
+# when we import the whole module we need to define -->
+# the name of the document as well as the class
 print(my_beetle.get_descriptive_name())
+# we can call the method as usual
 
 my_tesla = car.Electric_car('tesla', 'roadster', 2016)
 print(my_tesla.get_descriptive_name())
+
+## to import all classes from a module:
+# we write --> from module_name import * <--
+# not recommended bcs unclear whicih classes are being used
+# errors if the import has the same name as something else in the file
