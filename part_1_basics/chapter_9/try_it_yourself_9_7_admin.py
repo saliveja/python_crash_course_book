e58d71b7ecd78464d934270511b045a8b1cbde1c
saliveja <esman@riseup.net>
2022-02-25 10:43:04 +0100

    try it yourself 9-7 notes

--- part_1_basics/chapter_9/try_it_yourself_9_7_admin.py
+++ part_1_basics/chapter_9/try_it_yourself_9_7_admin.py
@@ -1,9 +1,7 @@
 class User():
     """making a user profile"""
 
-    def __init__(
-            self, first_name, last_name
-    ):
+    def __init__(self, first_name, last_name):
         self.first = first_name
         self.last = last_name
 
@@ -18,12 +16,24 @@ class User():
 class Admin(User):
     """Making an admin user."""
 
-    def __init__(
-            self, first_name, last_name
-    ):
+    def __init__(self, first_name, last_name):
+        """Initializing inheritance."""
         super().__init__(first_name, last_name)
-        self.privileges = privileges
-        privileges = ['can add post', 'can delete post', 'can ban user',
-                      'can give privileges']
 
-def show_privileges(self, privileges):
+    def show_privileges(self, privileges):
+        self.privileges = privileges
+        print(f"As {self.first.title()}, you are authorized to:")
+        for privilege in self.privileges:
+            print(privilege)
+
+
+list_privileges = ['can add post', 'can delete post', 'can ban user',
+                   'can give privileges']
+
+admin_privileges = Admin('Admin', '')
+admin_privileges.show_privileges(list_privileges)
+# 1. list at the end
+# 2. make a variable for the class and if inheritance:
+# add parameters in parenthesis
+# 3. variable from point 2, method you want to call and
+# within parenthesis the variable that contains the arguments/values
