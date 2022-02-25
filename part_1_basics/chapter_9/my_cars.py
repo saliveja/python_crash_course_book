b14e08c7803a5df31d12eb3ee413ea60b34055db
saliveja <esman@riseup.net>
2022-02-25 13:51:24 +0100

    importing modules

--- part_1_basics/chapter_9/my_cars.py
+++ part_1_basics/chapter_9/my_cars.py
@@ -1,3 +1,4 @@
+# importing classes
 from car import Car, Electric_car
 
 # from the file car, two classes are imported
@@ -11,3 +12,12 @@ my_tesla = Electric_car('tesla', 'roadster', 2016)
 # positional arguments for tesla
 print(my_tesla.get_descriptive_name())
 # calling the method in class Car
+
+# importing modules
+import car
+
+my_beetle = car.Car('volkswagen', 'beetle', 2016)
+print(my_beetle.get_descriptive_name())
+
+my_tesla = car.Electric_car('tesla', 'roadster', 2016)
+print(my_tesla.get_descriptive_name())
