59bc8579fabec023c91f5278141258199222ff6f
saliveja <esman@riseup.net>
2022-02-26 08:33:36 +0100

    calling method from imported class

--- part_1_basics/chapter_9/try_it_yourself_9_10_imported_restaurant.py
+++ part_1_basics/chapter_9/try_it_yourself_9_10_imported_restaurant.py
@@ -1,5 +1,9 @@
+# importing the class 'Restaurant' from module 'try_it...'
 from try_it_yourself_9_2_three_restaurants import Restaurant
 
 new_restaurant = Restaurant("lenas", "pancakes", 1600)
+# creating a variable and new arguments
 new_restaurant.describe_restaurant()
+# calling the method with the name and food of the restaurant
 new_restaurant.open_restaurant()
+# calling the method printing how long the restaurant is open
