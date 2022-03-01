d413bcdc67cc105c3608bbfcc88f00463d55d875
saliveja <esman@riseup.net>
2022-03-01 09:17:54 +0100

    creating more exits

--- part_1_basics/chapter_9/try_it_yourself_9_13_dice.py
+++ part_1_basics/chapter_9/try_it_yourself_9_13_dice.py
@@ -7,12 +7,13 @@ class Dice():
     def __init__(self, sides=6):
         """Initializing the dice class."""
         self.sides = sides
-        # creating the parameter sides with default value
+        # creating the parameter sides with default value 6
 
     def roll_die(self):
         """Rolling the die."""
         message = f"The die has {self.sides} sides. " \
-                  f"Press enter to roll the die"
+                  f"Press enter to roll the die.\n" \
+                  f"To quit press 'q'----->    "
         enter = input(message)
         x = int(self.sides) + 1
         # making the range 1, whatever argument is given for sides
@@ -20,26 +21,36 @@ class Dice():
             # if the user enters space, it returns a number
             range = randrange(1, x)
             return range
-        else:
-            print("no")
+        elif enter.lower() == 'q':
+            print("See you!")
             quit()
+            # the user can quit the program by entering 'q'
+            # a message is printed when choosing to exit
+        else:
+            print("Error. Try again.")
+            quit()
+            # if anything else than enter is
 
     def result(self):
         """Printing result"""
         res = self.roll_die()
-        print(f"The result is: {res}")
+        print(f"\nThe result is: {res}\n")
         # this method shows both roll_die() and results()
         # it prints the random number and the message
 
 
-roll = Dice(6)
-# die with six sides
-roll.result()
-
 roll = Dice(10)
-# dies with ten sides
-roll.result()
+# die with six sides
 
-roll = Dice(20)
-# die with twenty sides
-roll.result()
+while True:
+    roll.result()
+    # using a loop so the user can continue the
+    # program for as long as they want
+
+# roll = Dice(10)
+# # dies with ten sides
+# roll.result()
+#
+# roll = Dice(20)
+# # die with twenty sides
+# roll.result()
