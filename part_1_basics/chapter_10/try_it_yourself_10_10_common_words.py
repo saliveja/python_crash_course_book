a864d81553c0ce56162938121787101566360d17
saliveja <esman@riseup.net>
2022-03-05 15:21:09 +0100

    adding search possibilites

--- part_1_basics/chapter_10/try_it_yourself_10_10_common_words.py
+++ part_1_basics/chapter_10/try_it_yourself_10_10_common_words.py
@@ -13,9 +13,21 @@ def count_words(filename, word):
         number = contents.count(word)
         # 3389 without space
         # 2276 with space
-        print(f"The file {filename} contains {number} of the word: 'the'.")
+        print(f"The file {filename} contains {number} of the word: {word}.")
 
 
-word = 'the'
-filename = 'immortals_without_a_god.txt'
-count_words(filename, word)
+# word = 'the'
+filenames = ['immortals_without_a_god.txt', 'alice.txt', 'siddharta.txt',
+             'moby_dick.txt', 'little_women.txt']
+
+count_words(filenames[0], 'mountain')
+count_words(filenames[1], 'love')
+count_words(filenames[3], 'class')
+# using index to choose which file from the list to search
+# second argument defines the word
+
+word = 'she'
+for filename in filenames:
+    count_words(filename, word)
+    # for loop is more useful if we want to search for the
+    # same word in all files
