59308b62306e844f8db1eb34bc11cf92da6b1a75
saliveja <esman@riseup.net>
2022-03-05 15:26:01 +0100

    adding lower()

--- part_1_basics/chapter_10/try_it_yourself_10_10_common_words.py
+++ part_1_basics/chapter_10/try_it_yourself_10_10_common_words.py
@@ -10,7 +10,10 @@ def count_words(filename, word):
         pass
 
     else:
-        number = contents.count(word)
+        number = contents.lower().count(word)
+        # text is case sensitive and we need to use lower()
+        # to get the accurate number
+        # lower() and count() can go next to each other
         # 3389 without space
         # 2276 with space
         print(f"The file {filename} contains {number} of the word: {word}.")
@@ -20,13 +23,13 @@ def count_words(filename, word):
 filenames = ['immortals_without_a_god.txt', 'alice.txt', 'siddharta.txt',
              'moby_dick.txt', 'little_women.txt']
 
-count_words(filenames[0], 'mountain')
-count_words(filenames[1], 'love')
-count_words(filenames[3], 'class')
+# count_words(filenames[0], 'mountain')
+# count_words(filenames[1], 'love')
+# count_words(filenames[3], 'class')
 # using index to choose which file from the list to search
 # second argument defines the word
 
-word = 'she'
+word = 'alice'
 for filename in filenames:
     count_words(filename, word)
     # for loop is more useful if we want to search for the
