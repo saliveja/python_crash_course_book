cf04502f73e7d8b77ec79c1fa56fbfae4ab9539d
saliveja <esman@riseup.net>
2022-03-06 09:45:12 +0100

    try, except block with json

--- part_1_basics/chapter_10/remember_me.py
+++ part_1_basics/chapter_10/remember_me.py
@@ -1,8 +1,22 @@
 import json
 
-username = input("What is your name? ")
-
 filename = 'username.json'
-with open(filename, 'w') as f:
-    json.dump(username, f)
-    print(f"We'll remember you when you come back, {username}!")
+
+try:
+    with open(filename) as f:
+        username = json.load(f)
+        # trying if the input was made before and if so, it will be loaded
+        # opening in read mode
+
+except FileNotFoundError:
+    # if the file is not found, the user will be asked to give input
+    username = input("What is your name? ")
+    with open(filename, 'w') as f:
+        # opening in write mode which will create the file as well
+        json.dump(username, f)
+        # input is saved in the the file
+        print(f"We'll remember you when you come back, {username}!")
+
+else:
+    print(f"Welcome back, {username}!")
+    # if the data already exists, this message will be printed
