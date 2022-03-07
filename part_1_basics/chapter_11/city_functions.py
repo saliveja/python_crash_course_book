039dc635f33691847d0ce2fac30459f9aa0ad775
saliveja <esman@riseup.net>
2022-03-07 11:28:16 +0100

    unittest to test function

---
+++ part_1_basics/chapter_11/city_functions.py
@@ -0,0 +1,5 @@
+def city_country(city, country):
+    """Printing city and country name."""
+
+    name_city_country = f"{city}, {country}"
+    return name_city_country.title()
---
+++ part_1_basics/chapter_11/test_cities.py
@@ -0,0 +1,16 @@
+import unittest
+from city_functions import city_country
+
+
+class CityCountryTestcase(unittest.TestCase):
+    """Test for city_functions.py"""
+
+    def test_city_country(self):
+        """Does city/country like Oslo, Norway work?"""
+
+        name_city_country = city_country('oslo', 'norway')
+        self.assertEqual(name_city_country, 'Oslo, Norway')
+
+
+if __name__ == '__main__':
+    unittest.main()
