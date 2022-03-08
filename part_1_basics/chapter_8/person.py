c9449653d8193d93220bab90af5d1dbf5b6dda7f
saliveja <esman@riseup.net>
2022-03-08 10:01:42 +0100

    using unittest to check program

--- part_1_basics/chapter_8/person.py
+++ part_1_basics/chapter_8/person.py
@@ -1,13 +1,20 @@
 # returning a dictionary
-def build_person(first_name, last_name, age=''):
-    """Return a dictionary of information about a person."""
-    person = {'first': first_name, 'last': last_name}
-    # creating a dictionary where the value is the parameter
-    if age:
-        person['age'] = age
-        # if age in included in the dictionary person
-    return person
 
+class Dictionary:
+    """Creating a dictionary."""
 
-musician = build_person('jimi', 'hendrix', age=27)
-print(musician)
+    def __init__(self, first_name, last_name, person, age=''):
+        self.first = first_name
+        self.last = last_name
+        self.age = age
+        self.person = person
+
+    def build_person(self):
+        """Return a dictionary of information about a person."""
+
+        self.person = self.first + self.last
+        # creating a dictionary where the value is the parameter
+        if self.age:
+            self.person['age'] = self.age
+            # if age in included in the dictionary person
+        return self.person
