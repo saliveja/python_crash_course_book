ca8f8939631fc49df77f96fb4409dc5da041c79b
saliveja <esman@riseup.net>
2022-03-09 19:36:14 +0100

    class for ship - image, access and  position

--- part_1_basics/projects/alien_invasion/READ_ME.md
+++ part_1_basics/projects/alien_invasion/READ_ME.md
@@ -1,4 +1,6 @@
-### READ_ME for the game Alien invasion
+### Alien invasion
+
+#### How does the game work
 
 * Player controls rocketship from bottom center of the screen
 * The player can move the ship using left and right arrows
@@ -9,8 +11,10 @@
 * If an alien reaches the bottom or hits a ship, the player loses a ship
 * If the player loses three ships it's game over.
 
-#### Resources
+#### Resources to do a game
 
 * freely licenses graphics that can be used and modified, can be found:
   pixabay.com
 * easiest to use .bmp file in a game because pygame loads it by default
+* game elements are all treated like rectangles
+* 
--- part_1_basics/projects/alien_invasion/ship.py
+++ part_1_basics/projects/alien_invasion/ship.py
@@ -4,11 +4,21 @@ import pygame
 class Ship:
     """A class to manage the ship."""
 
-    def __init__(self):
+    def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
 
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
-
+        # placing the ship in its current location
         self.image = pygame.image.load('images/ship.bmp')
+        # loading the image
         self.rect = self.image.get_rect()
+        # accessing the ships surface rect attribute
+        # to be able to position later
+        self.rect.midbottom = self.screen_rect.midbottom
+        # ships position specified to bottom center
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
+        # position (0, 0) means the upper left corner
