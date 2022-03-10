84d41909db2e806e877d9ec3a0e97b74c2e22b7a
saliveja <esman@riseup.net>
2022-03-10 08:39:19 +0100

    restructuring code

--- part_1_basics/projects/alien_invasion/alien_invasion.py
+++ part_1_basics/projects/alien_invasion/alien_invasion.py
@@ -2,8 +2,8 @@
 import sys
 # used for the functionality of the game
 import pygame
-
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -26,6 +26,10 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         # assigns the name Alien Invasion to the game
 
+        self.ship = Ship(self)
+        # making an instance of the ship
+        # self refers to the current instance of AlienInvasion
+
         self.bg_color = (230, 230, 230)
         # setting the background color
         # colors in Pygam are RGB colors
@@ -34,22 +38,33 @@ class AlienInvasion:
         """Start the main loop for the game"""
 
         while True:
-            for event in pygame.event.get():
-                # an event is the action of a user
-                # pygame.event.get() returns returns a list of events
-                # that have taken place since last time the loop was called
-                if event.type == pygame.QUIT:
-                    # if the user clicks on the game windows close button
-                    # the user exits the game
-                    sys.exit()
+            self._check_events()
 
-            self.screen.fill(self.settings.bg_color)
-            # filling the screen with background color
-            # takes only one argument
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
 
-            pygame.display.flip()
-            # Makes the most recent screen visible
-            # continously updating
+        for event in pygame.event.get():
+            # an event is the action of a user
+            # pygame.event.get() returns returns a list of events
+            # that have taken place since last time the loop was called
+            if event.type == pygame.QUIT:
+                # if the user clicks on the game windows close button
+                # the user exits the game
+                sys.exit()
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+
+        self.screen.fill(self.settings.bg_color)
+        # filling the screen with background color
+        # takes only one argument
+
+        self.ship.blitme()
+        # drawing the ship on the screen
+
+        pygame.display.flip()
+        # Makes the most recent screen visible
+        # continuously updating
 
 
 if __name__ == '__main__':
--- part_1_basics/projects/alien_invasion/settings.py
+++ part_1_basics/projects/alien_invasion/settings.py
@@ -4,6 +4,6 @@ class Settings:
     def __init__(self):
         """Initializing the games setting."""
 
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 1900
+        self.screen_height = 1000
         self.bg_color = (230, 230, 230)
