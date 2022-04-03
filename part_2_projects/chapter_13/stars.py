6a44fdcb78c07e991c26192384e7faac2b1d7498
saliveja <esman@riseup.net>
2022-04-03 14:57:19 +0200

    creating a class for the stars

---
+++ part_2_projects/chapter_13/stars.py
@@ -0,0 +1,23 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Ship(Sprite):
+    """A class to manage the ship."""
+
+    def __init__(self, stars_main):
+        """Initialize the ship and set its starting position."""
+
+        super().__init__()
+        # Ship inherits from Sprite
+
+        self.screen = stars_main.screen
+        self.screen_rect = stars_main.screen.get_rect()
+        self.image = pygame.image.load('images/star.png')
+        self.rect = self.image.get_rect()
+        self.rect.topleft = self.screen_rect.topleft
+        self.x = float(self.rect.x)
+
+    def update(self):
+        """Drawing and updating the stars on the screen."""
+        self.screen.blit(self.image, self.rect)
--- part_2_projects/chapter_13/try_it_yourself_13_1_stars.py
+++ part_2_projects/chapter_13/try_it_yourself_13_1_stars.py
@@ -1,1 +1,23 @@
- 
+import pygame
+
+
+class EmptyScreen:
+
+    def __init__(self, ):
+        """Initializing empty screen."""
+        pygame.init()
+        self.bg_color = (100, 30, 200)
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        pygame.display.set_caption("Stars")
+
+        self.rect = self.image.get_rect()
+
+    def run_program(self):
+        """running the program."""
+        while True:
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    es = EmptyScreen()
+    es.run_program()
