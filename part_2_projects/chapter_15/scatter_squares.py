5252e949344d56d5ef28375c7e0db726911eb915
saliveja <esman@riseup.net>
2022-04-09 10:25:44 +0200

    using range and for loop to plot

--- part_2_projects/chapter_15/scatter_squares.py
+++ part_2_projects/chapter_15/scatter_squares.py
@@ -1,19 +1,25 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
+x_values = range(1, 1001)
 # x values are the numbers to be squared
-y_values = [1, 4, 9, 16, 25]
+# range is 1-1000
+y_values = [x ** 2 for x in x_values]
 # contains the square of each number
-# the points to be plotted:
-# (1,1), (2,4), (3,9) etc
+# making a for loop for the calculation
+# x**2 is square
+# results stored in y_values
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=200)
-# 200 is the size of the dots
+ax.scatter(x_values, y_values, s=10)
+# s variable defines the size of the dots
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 
 ax.set_ylabel("Square of Value", fontsize=14)
 ax.tick_params(axis='both', which='major', labelsize=14)
+ax.axis([0, 1100, 0, 1100000])
+# axis method requires four values
+# the minimum and the maximum values for the x and y axis
+# specifying the range for each axis
 plt.show()
