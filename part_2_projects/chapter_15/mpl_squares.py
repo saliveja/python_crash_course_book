06f8bd72add117dd79733ead915c984b9b76911b
saliveja <esman@riseup.net>
2022-04-09 09:33:17 +0200

    adding style

--- part_2_projects/chapter_15/mpl_squares.py
+++ part_2_projects/chapter_15/mpl_squares.py
@@ -1,11 +1,15 @@
 import matplotlib.pyplot as plt
 
 input_values = [1, 2, 3, 4, 5]
-# plot assumes that that the first data point is 0
+# plot assumes that the first data point is 0
 # this is the default behavior
 # by adding this variable, we override the default
 squares = [1, 4, 9, 16, 25]
 # list containing a square number sequence
+plt.style.use('seaborn')
+# >>> import matplotlib.pyplot as plt
+# >>> plt.style.available
+# to see the available styles
 fig, ax = plt.subplots()
 # subplots() exists within pyplot
 # can generate one or more plots within the same figure
