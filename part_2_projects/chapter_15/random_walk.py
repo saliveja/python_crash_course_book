f5f5493e8e0a393db01ab5196da81eb3ad4196ce
saliveja <esman@riseup.net>
2022-04-09 13:19:12 +0200

    creating class for random choices

--- part_2_projects/chapter_15/random_walk.py
+++ part_2_projects/chapter_15/random_walk.py
@@ -8,6 +8,36 @@ class RandomWalk:
         """initialize attributes of a walk."""
 
         self.num_points = num_points
-        self.x_vales = 0
-        self.y_values = 0
+        # default number of points in a walk is 5000
+        self.x_values = [0]
+        self.y_values = [0]
         # all walks start at 0
+
+    def fill_walk(self):
+        """Calculate all the poiunts in the walk."""
+
+        while len(self.x_values) < self.num_points:
+            # while the length of self.x_values is less than 5000
+            x_direction = choice([1, -1])
+            # the choice of direction is -1 left or 1 right
+            x_distance = choice([0, 1, 2, 3, 4])
+            # the distance is within range 0-5
+            x_step = x_direction * x_distance
+            # one step is direction multiplied with the distance
+
+            y_direction = choice([1, -1])
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
+            # y axis had the same possibility of choices as x axis
+
+            if x_step == 0 and y_step == 0:
+                continue
+
+            x = self.x_values[-1] + x_step
+            # [-1] refers to index and means the last input in the list
+            y = self.y_values[-1] + y_step
+
+            self.x_values.append(x)
+            # adding x to the list
+            self.y_values.append(y)
+            # adding y to the list
