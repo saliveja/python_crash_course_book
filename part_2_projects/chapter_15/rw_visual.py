0b3b98a17f12a9c7593786365360e912584d9a43
saliveja <esman@riseup.net>
2022-04-10 09:57:30 +0200

    adding color to points

--- part_2_projects/chapter_15/rw_visual.py
+++ part_2_projects/chapter_15/rw_visual.py
@@ -11,8 +11,14 @@ while True:
     # using the style classic
     fig, ax = plt.subplots()
     # variable representing the whole figure and a single plot
-    ax.scatter(rw.x_values, rw.y_values, s=15)
-    # calling for the lists (with default 0) in RandomWalk
+    point_numbers = range(rw.num_points)
+    # referring to the variable in RandomWalk, defining the number to 5000
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+               edgecolors='none', s=15)
+    # c=point_numbers sayd that the points in RandomWalk should be colored
+    # the chosen color is blue
+    # ew.x_values etc is the list starting with default value 0
+    # removing edge color
     plt.show()
 
     keep_running = input("Make another walk? y/n: ")
