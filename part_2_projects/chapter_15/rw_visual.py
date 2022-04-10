2f36f5430921a883a3f368fd8b80932f9040b11b
saliveja <esman@riseup.net>
2022-04-10 09:22:22 +0200

    adding notes to project

--- part_2_projects/chapter_15/rw_visual.py
+++ part_2_projects/chapter_15/rw_visual.py
@@ -4,8 +4,12 @@ from random_walk import RandomWalk
 
 rw = RandomWalk()
 rw.fill_walk()
+# making an instance of the class RandomWalk
 
 plt.style.use('classic')
+# using the style classic
 fig, ax = plt.subplots()
+# variable representing the whole figure and a single plot
 ax.scatter(rw.x_values, rw.y_values, s=15)
+# calling for the lists (with default 0) in RandomWalk
 plt.show()
