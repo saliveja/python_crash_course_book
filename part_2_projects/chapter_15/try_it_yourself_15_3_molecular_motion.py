187a85aae8941373941e04034ab6f96730e78408
saliveja <esman@riseup.net>
2022-04-10 10:52:41 +0200

    using plot() for RandomWalk instead of scatter()

--- part_2_projects/chapter_15/try_it_yourself_15_3_molecular_motion.py
+++ part_2_projects/chapter_15/try_it_yourself_15_3_molecular_motion.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 while True:
-    rw = RandomWalk(50_000)
+    rw = RandomWalk()
     # increasing the number of points to 50 000 from 5000
     rw.fill_walk()
     # making an instance of the class RandomWalk
@@ -16,17 +16,15 @@ while True:
     # more efficient use of available space
     point_numbers = range(rw.num_points)
     # referring to the variable in RandomWalk, defining the number to 5000
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
-               edgecolors='none', s=1)
+    ax.plot(rw.x_values, rw.y_values, c='pink', linewidth=0.75)
     # c=point_numbers said that the points in RandomWalk should be colored
     # the chosen color is blue
     # ew.x_values etc is the list starting with default value 0
     # removing the black edge of each point
-    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(0, 0, c='purple', s=100)
     # set the starting point to 0, 0 with color green and in size 100
     # instead of 15
-    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red',
-               edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='blue', s=100)
     # the last point, which is defined by the index -1, is red and size 100
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
