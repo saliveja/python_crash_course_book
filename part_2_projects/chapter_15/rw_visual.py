5de45cf82fff31a5aa3b205e51baacbb08377991
saliveja <esman@riseup.net>
2022-04-10 10:06:36 +0200

    removing both axis

--- part_2_projects/chapter_15/rw_visual.py
+++ part_2_projects/chapter_15/rw_visual.py
@@ -25,6 +25,9 @@ while True:
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red',
                edgecolors='none', s=100)
     # the last point, which is defined by the index -1, is red and size 100
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    # removing both axis
     plt.show()
 
     keep_running = input("Make another walk? y/n: ")
