13697861327d1a82d0a2da50d51413dd6066e168
saliveja <esman@riseup.net>
2022-04-11 10:41:11 +0200

    three d6 1000 times

---
+++ part_2_projects/chapter_15/try_it_yourself_15_7_three_dice.py
@@ -0,0 +1,45 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+from die import Die
+
+die_1 = Die()
+die_2 = Die()
+die_3 = Die()
+# creating three instances of class Die
+results = []
+
+for roll_num in range(1000):
+    result = die_1.roll() + die_2.roll() + die_3.roll()
+    # we have two dice instead of one and the result is the sum of both
+    results.append(result)
+
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides + die_3.num_sides
+# making a variable including both die in the for loop
+for value in range(2, max_result + 1):
+    # 2 is the smallest possible result
+    # max_result is the result of the sides of both dice
+    # +1 is so that the loop will go through each number
+    frequency = results.count(value)
+    # counting how many times the value is selected
+    frequencies.append(frequency)
+    # appending the count to the list frequencies
+
+x_values = list(range(3, max_result + 1))
+# x_values is the range of the two dice
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Result', 'dtick': 1}
+# dtick tells plotly to label every tick mark
+y_axis_config = {'title': 'Frequency of result'}
+# title on the y axis is Frequency of result
+my_layout = Layout(title='Results of rolling three D6 1000 times',
+                   xaxis=x_axis_config, yaxis=y_axis_config)
+# layout id the overall visual of the graph
+# title is the overall title of the graph
+# xaxis and yaxis are the side labels
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6_d6.html')
+# offline needs a dictionary containing the data
+# and accepts a file name where we will store the output
+
+# print(frequencies)
