14f7a15a9079ffa367c4de5343a671a784ea993c
saliveja <esman@riseup.net>
2022-04-11 08:59:51 +0200

    adding notes

--- part_2_projects/chapter_15/die_visual.py
+++ part_2_projects/chapter_15/die_visual.py
@@ -1,10 +1,17 @@
 from die import Die
 
 die = Die()
+# creating an instance of class Die
 results = []
+# making a list to store results
 
 for roll_num in range(100):
+    # rolling the die a hundred times
     result = die.roll()
+    # the result is the method in Die called roll
+    # the method only returns a random result between one and number of sides
+    # int his case six
     results.append(result)
+    # adding the result to the list
 
 print(results)
