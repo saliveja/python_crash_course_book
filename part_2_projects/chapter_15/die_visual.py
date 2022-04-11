757102a394d9a1d0268c8919fc084b19e443f259
saliveja <esman@riseup.net>
2022-04-11 09:06:59 +0200

    counting frequency of numbers

--- part_2_projects/chapter_15/die_visual.py
+++ part_2_projects/chapter_15/die_visual.py
@@ -5,7 +5,7 @@ die = Die()
 results = []
 # making a list to store results
 
-for roll_num in range(100):
+for roll_num in range(1000):
     # rolling the die a hundred times
     result = die.roll()
     # the result is the method in Die called roll
@@ -14,4 +14,15 @@ for roll_num in range(100):
     results.append(result)
     # adding the result to the list
 
-print(results)
+frequencies = []
+# storing the number of times each value is rolled
+
+for value in range(1, die.num_sides + 1):
+    # we loop through all numbers on the die
+    # +1 is so that the same number won't be counted every time
+    frequency = results.count(value)
+    # counting how many times the value is selected
+    frequencies.append(frequency)
+    # appending the count to the list frequencies
+
+print(frequencies)
