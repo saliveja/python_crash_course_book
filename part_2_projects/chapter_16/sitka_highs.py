c04f12f6e132e52c5c82dfeff5f737dfa99efeab
saliveja <esman@riseup.net>
2022-04-12 13:03:21 +0200

    creating graph from weather data

--- part_2_projects/chapter_16/sitka_highs.py
+++ part_2_projects/chapter_16/sitka_highs.py
@@ -1,4 +1,5 @@
 import csv
+import matplotlib.pyplot as plt
 
 filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
@@ -16,3 +17,21 @@ with open(filename) as f:
     for index, column_header in enumerate(header_row, +1):
         # enumerate returns the index and value for each item
         print(index, column_header)
+
+    highs = []
+    for row in reader:
+        high = int(row[6])
+        highs.append(high)
+
+print(highs)
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+ax.set_title('Daily high temperatures, July 2018', fontsize=24)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel('Temperature (F)', fontsize=16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
