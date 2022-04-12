6381037e885b64ed2ef7033d5c17ce5bc829436c
saliveja <esman@riseup.net>
2022-04-12 12:53:06 +0200

    creating index with the cathegories in data

--- part_2_projects/chapter_16/sitka_highs.py
+++ part_2_projects/chapter_16/sitka_highs.py
@@ -1,7 +1,18 @@
 import csv
 
-filename = 'sitka_weather_07-2018_simple.csv'
+filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
+    # the csv.reader() returns a reader object
+    # return a string each time the next method is called
     header_row = next(reader)
-    print(header_row)
+    # the header STATION represent the code for the weather station
+    # NAME indicates that the second value in each line is the name
+    # of the weather station
+    # The rest is the weather data: TMAX (high temperature),
+    # TMIN (low temperature)
+    # print(header_row)
+
+    for index, column_header in enumerate(header_row, +1):
+        # enumerate returns the index and value for each item
+        print(index, column_header)
