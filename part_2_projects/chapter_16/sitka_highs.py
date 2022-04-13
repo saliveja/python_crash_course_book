c73f484f060a1bba71a22e0a95249fd6f3af6f82
saliveja <esman@riseup.net>
2022-04-13 10:39:49 +0200

    adding low temperatures to the graph

--- part_2_projects/chapter_16/sitka_highs.py
+++ part_2_projects/chapter_16/sitka_highs.py
@@ -2,7 +2,7 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-filename = 'data/sitka_weather_07-2018_simple.csv'
+filename = 'data/sitka_weather_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     # the csv.reader() returns a reader object
@@ -15,31 +15,42 @@ with open(filename) as f:
     # TMIN (low temperature)
     # print(header_row)
 
-    for index, column_header in enumerate(header_row, +1):
+    for index, column_header in enumerate(header_row):
         # enumerate returns the index and value for each item
         print(index, column_header)
         # ie. 1 STATION
 
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
+    # creating two empty lists to store the dates and the high temperatures
     for row in reader:
-        current_date = datetime.strptime(row[3], '%Y-%m-%d')
-        high = int(row[6])
-        # this refers to point six in index which is high temperatures
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        # converting date to a datetime object
+        high = int(row[5])
+        # this refers to row 5 in index which is high temperatures (TMAX)
+        low = int(row[6])
+        # low equals the low temperatures (TMIN)
         dates.append(current_date)
+        # datetime object appended to list dates
         highs.append(high)
         # each are appended to the list highs
+        lows.append(low)
+        # appending the low temperatures to the list lows
 
 print(highs)
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(highs, c='red')
+ax.plot(dates, highs, c='red')
 # graph line is colored red
-
-ax.set_title('Daily high temperatures, July 2018', fontsize=24)
+# making graph of high temps
+ax.plot(dates, lows, c='blue')
+# adding dates and data for low temp in blue color
+ax.set_title('Daily high and low temperatures - 2018', fontsize=24)
 # overall title
 ax.set_xlabel('', fontsize=16)
 # x has no label
+fig.autofmt_xdate()
+# draws the date labels diagonally so they don't overlap
 ax.set_ylabel('Temperature (F)', fontsize=16)
 # y label displays temperatures
 ax.tick_params(axis='both', which='major', labelsize=16)
