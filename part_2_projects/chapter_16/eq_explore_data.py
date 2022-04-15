353a54f515ccf32261813e36c2b454dd7c76d93c
saliveja <esman@riseup.net>
2022-04-15 17:23:39 +0200

    adding longitude and latitude for the earthquakes

--- part_2_projects/chapter_16/eq_explore_data.py
+++ part_2_projects/chapter_16/eq_explore_data.py
@@ -15,14 +15,21 @@ all_eq_dicts = all_eq_data['features']
 # pulling out the number of earthquakes from the json file
 # print(len(all_eq_dicts))
 
-mags = []
+mags, lons, lats = [], [], []
+# lists: magnitude, longitude, latitude
 for eq_dict in all_eq_dicts:
     # for dictionary in all dictionaries
     mag = eq_dict['properties']['mag']
     # earthquake magnitude is stored in properties section
     # and under the key 'mag'
     # each magnitude is stored in the variable mag
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
     mags.append(mag)
     # and appended to the list mags
+    lons.append(lon)
+    lats.append(lat)
     print(mags[:10])
     # printing the first ten values
+    print(lons[:5])
+    print(lats[:5])
