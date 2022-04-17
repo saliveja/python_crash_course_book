f1f3463e832d358060fe4eef2e901c162483b2de
saliveja <esman@riseup.net>
2022-04-17 15:31:00 +0200

    printing value from selected keys

--- part_2_projects/chapter_17/python_repos.py
+++ part_2_projects/chapter_17/python_repos.py
@@ -9,9 +9,31 @@ headers = {'Accept': 'application/vnd.github.v3+json'}
 r = requests.get(url, headers=headers)
 # calling get(), passing url and headers
 print(f"Status code: {r.status_code}")
-# status code tells us if it was successful or not
+# status code 200 tells us that the request was successful
 
 response_dict = r.json()
 # Converting the json input to a dictionary
-print(response_dict.keys())
-# printing keys from the response
+print(f"Total repositories: {response_dict['total_count']}")
+# printing values from key 'total_count'
+# total_count is the number of python repos on github
+
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+# printing the number of repositories returned
+
+repo_dict = repo_dicts[0]
+# first repo in variable repo_dict
+print(f"\nKeys: {len(repo_dict)}")
+# printing the number of keys in the repo --> 30
+for key in sorted(repo_dict.keys()):
+    print(key)
+    # there are 78 keys in this repo
+
+print("\nSelected information about first repository:")
+print(f"name: {repo_dict['name']}")
+print(f"Owner: {repo_dict['owner']['login']}")
+print(f"Stars: {repo_dict['stargazers_count']}")
+print(f"Repository: {repo_dict['html_url']}")
+print(f"Created: {repo_dict['created_at']}")
+print(f"Updated: {repo_dict['updated_at']}")
+print(f"Description: {repo_dict['description']}")
