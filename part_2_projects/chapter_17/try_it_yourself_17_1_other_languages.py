c49a1881eafb5043b69ae1e99dac534012e4743b
saliveja <esman@riseup.net>
2022-04-19 14:17:51 +0200

    two functions for test completed

--- part_2_projects/chapter_17/try_it_yourself_17_1_other_languages.py
+++ part_2_projects/chapter_17/try_it_yourself_17_1_other_languages.py
@@ -7,12 +7,13 @@ r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
 
 response_dict = r.json()
-total_count = response_dict
+total_count = response_dict['total_count']
 print(f"Total repositories: {response_dict['total_count']}")
 repo_dicts = response_dict['items']
 print(f"Repositories returned: {len(repo_dicts)}")
 
 repo_dict = repo_dicts[0]
+keys = len(repo_dict)
 print(f"\nKeys: {len(repo_dict)}")
 for key in sorted(repo_dict.keys()):
     print(key)
--- part_2_projects/chapter_17/try_it_yourself_17_3_testing_python_repos.py
+++ part_2_projects/chapter_17/try_it_yourself_17_3_testing_python_repos.py
@@ -1,26 +1,25 @@
 import unittest
+import requests as r
 import try_it_yourself_17_1_other_languages as ol
 
 
-class CheckValue(unittest.Testcase):
+class CheckValue(unittest.TestCase):
     """test for try_it_yourself_17_1_other_languages.py"""
-
-    def __init__(self):
-        """Initializing test class"""
 
     def test_status_code(self):
         """Asserting value of status code to 200"""
-
-        self.assertEqual(ol.r.status_code, 200)
+        status_code = ol.r.status_code
+        self.assertEqual(status_code, 200)
 
     def test_number_of_items(self):
-        """Testing if the number items are as expected"""
-        self.assertEqual(ol.repo_dicts, 30)
+        """Testing if the number keys are as expected"""
+        self.assertEqual(ol.keys, 78)
 
     def testing_number_of_repositories(self):
         """Testing so the number of repositories are greater
         than a certain amount"""
-        self.assertEqual(ol.total_count, 994678)
+        if ol.repo_dicts >= 992000:
+            self.assertE == True
 
 
 if __name__ == '__main__':
